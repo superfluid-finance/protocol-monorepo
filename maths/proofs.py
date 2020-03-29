f8616e5892aa33a1d5a6c26c0a935e9cf6f217be
Miao ZhiCheng <miao@decentral.ee>
2020-03-29 15:54:30 +0300

    add maths proofs for flowswap exchange

---
+++ maths/proofs.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+# Part of Superfluid Contracts. See LICENSE file for full copyright and licensing details.
+
+"""
+This module uses sagemath to solve the equations for both uniswap and flowswap
+type of exchanges.
+"""
+
+from sage.all import var, function, solve
+from sage.symbolic.integration.integral import definite_integral
+
+# @description CLP : Constant Liquidity Product
+def CLP(initialLa, initialLb, newLa, newLb):
+    return newLa * newLb == initialLa * initialLb
+
+def solveUniswap():
+    print "==== Uniswap Equations ===="
+    L_a, Delta_a, L_b, Delta_b = var("L_a","Delta_a", "L_b", "Delta_b")
+
+    liquidityEquation = CLP(
+        L_a,
+        L_b,
+        L_a + Delta_a,
+        L_b + Delta_b
+    )
+    print "Liquidity Equation: \t", liquidityEquation
+
+    solutions_Delta_b = solve(liquidityEquation, Delta_b)
+    assert len(solutions_Delta_b) == 1
+    Delta_b1 = solutions_Delta_b[0].right()
+    print "Output equation:    \t", Delta_b1
+
+    Price = - Delta_b1 / Delta_a
+    print "Price equation:     \t", Price
+
+def solveFlowswap():
+    print "==== Flowswap Equations ===="
+    L_a, L_b, T, t, q = var("L_a", "L_b", "T", "t", "q")
+    f_a = function("f_a", nargs = 1)
+    f_b = function("f_b", nargs = 1)
+
+    liquidityEquation = CLP(
+        L_a,
+        L_b,
+        L_a + f_a(t) + q * f_b(t),
+        L_b + f_b(t) + 1/q * f_a(t)
+    )
+    print "Liquidity Equation: \t", liquidityEquation
+
+    solutions_q = solve(liquidityEquation, q)
+    print "q function solutions: \t", solutions_q
+
+    q1 = solutions_q[0].right()
+    Delta_a = f_a(t) + q1 * f_b(t)
+    Delta_b = f_b(t) + 1/q1 * f_a(t)
+    Price = - Delta_b / Delta_a
+    print "Price equation:       \t", Price
+
+solveUniswap()
+solveFlowswap()

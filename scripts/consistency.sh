cef32173c0392ea8d343f673f27df1be7345a242
Axe <ngsmo@iscte-iul.pt>
2020-05-08 23:55:55 +0100

    Print logs

--- scripts/consistency.sh
+++ scripts/consistency.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 echo "starting consistency tests";
+echo "Cleaning ABI";
+rm -rf ../build/contracts/*.js;
+truffle build;
+
 for i in `seq $1`;
-    do 
+    do
         echo "$i/$1...";
-        truffle test ../test/*.js | grep Error;
+        #truffle test ../test/*.js | grep Error;
+        truffle test ../test/*.js;
 done

22832dc121cc6427e1d274050cf296ef304c2c79
Axe <ngsmo@iscte-iul.pt>
2020-05-09 20:37:02 +0100

    Better report

--- scripts/consistency.sh
+++ scripts/consistency.sh
@@ -7,6 +7,6 @@ truffle build;
 for i in `seq $1`;
     do
         echo "$i/$1...";
-        #truffle test ../test/*.js | grep Error;
+        #truffle test ../test/*.js;
         truffle test ../test/*.js | grep "Contract\|#\|Error";
 done

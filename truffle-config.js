495512f8d4bee5247f26e66adc0689921e334f96
Axe <ngsmo@iscte-iul.pt>
2020-05-10 15:45:54 +0100

    lint

--- truffle-config.js
+++ truffle-config.js
@@ -142,7 +142,7 @@ module.exports = {
     // Set default mocha options here, use special reporters etc.
     mocha: {
         // timeout: 100000
-        reporter: 'eth-gas-reporter'
+        reporter: "eth-gas-reporter"
     },
 
     // Configure your compilers

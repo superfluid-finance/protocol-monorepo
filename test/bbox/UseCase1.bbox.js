1237f7f604000a8afb7f795d21937b34cc20b91d
Axe <ngsmo@iscte-iul.pt>
2020-05-17 18:47:49 +0100

    Update Use Case 1

--- test/bbox/UseCase1.bbox.js
+++ test/bbox/UseCase1.bbox.js
@@ -4,7 +4,7 @@ const FlowAgreement = artifacts.require("FlowAgreement");
 
 const {
     web3tx,
-    //wad4human,
+    wad4human,
     toWad
 } = require("@decentral.ee/web3-test-helpers");
 
@@ -15,9 +15,10 @@ contract("Usecase 1 Stories", accounts => {
     const MAX_UINT256 = "115792089237316195423570985008687907853269984665640564039457584007913129639935";
 
     const admin = accounts[0];
-    const user1 = accounts[1];
-    const user2 = accounts[2];
-    const user3 = accounts[3];
+    const Miao = accounts[1];
+    const Fran = accounts[2];
+    const Mike = accounts[3];
+    const Nuno = accounts[4];
 
     let token;
     let superToken;
@@ -35,8 +36,9 @@ contract("Usecase 1 Stories", accounts => {
                 from: admin
             });
 
-        await token.mint(user1, toWad(1000));
-        await token.mint(user2, toWad(1000));
+        await token.mint(Miao, toWad(10));
+        await token.mint(Fran, toWad(10));
+        await token.mint(Mike, toWad(10));
 
         superToken = await web3tx(SuperToken.new, "SuperToken.new")(
             token.address,
@@ -46,66 +48,87 @@ contract("Usecase 1 Stories", accounts => {
                 from: admin
             });
 
-        await web3tx(token.approve, "token.approve from user1 to SuperToken")(
+        await web3tx(token.approve, "token.approve from Miao to SuperToken")(
             superToken.address,
             MAX_UINT256, {
-                from: user1
+                from: Miao
             }
         );
 
-        await web3tx(token.approve, "token.approve from user2 to SuperToken")(
+        await web3tx(token.approve, "token.approve from Fran to SuperToken")(
             superToken.address,
             MAX_UINT256, {
-                from: user2
+                from: Fran
             }
         );
-        await web3tx(superToken.addAgreement, "SuperToken.addAgreement")(
-            agreement.address, {
-                from: admin
+
+
+        await web3tx(token.approve, "token.approve from Mike to SuperToken")(
+            superToken.address,
+            MAX_UINT256, {
+                from: Mike
             }
         );
     });
 
-    it("#1 - One user is receiving many flows", async () => {
+    it("#1 - Nuno getting rich (One user is receiving many flows)", async () => {
 
-        await superToken.upgrade(toWad(10), {from: user1});
-        await superToken.upgrade(toWad(5), {from: user2});
+        await superToken.upgrade(toWad(10), {from: Miao});
+        await superToken.upgrade(toWad(10), {from: Fran});
+        await superToken.upgrade(toWad(10), {from: Mike});
 
-        const flowRate1 = toWad(10 / 30 / 24 /3600);
-        const flowRate2 = toWad(5 / 30 / 24 / 3600);
+        const flowRate1 = "16534391534391"; // toWad(10) / 7 / 24 / 3600;
+        const flowRate2 = "8267195767195";   // toWad(5)  / 7 / 24 / 3600;
 
-
-        await web3tx(agreement.createFlow, "User1 -> User3 new Agreement")(
+        await web3tx(agreement.createFlow, "Miao -> Nuno new Agreement")(
             superToken.address,
-            user3,
+            Nuno,
             flowRate1, {
-                from: user1
+                from: Miao
             }
         );
 
-        await web3tx(agreement.createFlow, "User2 -> User3 new Agreement")(
+        await web3tx(agreement.createFlow, "Fran -> Nuno new Agreement")(
             superToken.address,
-            user3,
+            Nuno,
             flowRate2, {
-                from: user2
+                from: Fran
+            }
+        );
+
+        await web3tx(agreement.createFlow, "Mike -> Nuno new Agreement")(
+            superToken.address,
+            Nuno,
+            flowRate2, {
+                from: Mike
             }
         );
 
 
-        await traveler.advanceTime(3600 * 24 * 7);
+        await traveler.advanceTime(7 * 24 * 3600);
         await traveler.advanceBlock();
 
-        let balance = await superToken.balanceOf.call(user3);
+        let NunoBalance = await superToken.balanceOf.call(Nuno);
+        let MiaoBalance = await superToken.balanceOf.call(Miao);
+        let FranBalance = await superToken.balanceOf.call(Fran);
+        let MikeBalance = await superToken.balanceOf.call(Mike);
 
-        console.log("HERE ---- > User 3 balance: ", balance.toString());
+        console.log("Nuno Balance: ", wad4human(NunoBalance));
+        console.log("Miao Balance: ", wad4human(MiaoBalance));
+        console.log("Fran Balance: ", wad4human(FranBalance));
+        console.log("Mike Balance: ", wad4human(MikeBalance));
+
+        console.log("Nuno balance after one week is ", NunoBalance.toString());
+
         await web3tx(superToken.downgrade, "User 3 is downgrading tokens")(
-            balance, {
-                from: user3
+            NunoBalance, {
+                from: Nuno
             }
         );
 
-        let finalbalance = await token.balanceOf.call(user3);
-        assert.equal(balance.toString(), finalbalance.toString(), "User 3 final balance is not the flow balance");
-        console.log("User 3 Downgraded token balance: ", finalbalance.toString());
+        let NunoFinalBalance = await token.balanceOf.call(Nuno);
+        console.log("Nuno Token balance ", NunoFinalBalance.toString());
+
+        assert.equal(NunoBalance.toString(), NunoFinalBalance.toString(), "Nuno final balance is not the flow balance");
     });
 });

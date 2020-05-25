a923bb31c029fc5af3defccbba969bea25200c9a
Miao ZhiCheng <miao@decentral.ee>
2020-05-25 21:43:38 +0300

    npm publish package

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = {
     load: (provider) => {
         let contracts = {
             FlowPayment : TruffleContract(require("./build/contracts/FlowPayment.json")),
+            SuperToken : TruffleContract(require("./build/contracts/SuperToken.json")),
             IERC20 : TruffleContract(require("@openzeppelin/contracts/build/contracts/IERC20.json")),
         };
         Object.values(contracts).forEach(i => i.setProvider(provider));

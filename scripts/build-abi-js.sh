3ab7797c5b8a18afd69bef6428dfd9efbb568a1c
Miao ZhiCheng <miao@decentral.ee>
2020-05-29 14:11:32 +0300

    fix abi build script

--- scripts/build-abi-js.sh
+++ scripts/build-abi-js.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")/.."
 
-echo "if (typeof module === "undefined") module = {};"
+echo "if (typeof module === \"undefined\") module = {};"
 echo "Superfluid_ABI = module.exports = {"
 for i in IERC20 IFlowAgreement ISuperToken;do
     echo "    $i: $(jq -c '.abi' build/contracts/$i.json),"

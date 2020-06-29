fdf76981088487e59766e376dd915792d0456a18
Miao ZhiCheng <miao@decentral.ee>
2020-06-29 16:54:02 +0300

    make it upgradable

--- scripts/configs.js
+++ scripts/configs.js
@@ -5,5 +5,6 @@ module.exports = {
         resolverAddress: "0x6258d03724c90138baf05Ed7bb438a037C7bA6E4"
     },
     private: {
+        resolverAddress: process.env.PRIVATE_RESOLVER_ADDRESS,
     }
 };

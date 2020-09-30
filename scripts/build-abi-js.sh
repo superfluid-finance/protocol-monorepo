39e87c3d4f320b2cb9259b64812a49561306bfe8
Miao ZhiCheng <miao@decentral.ee>
2020-09-30 11:39:20 +0300

    change build:abi step

--- scripts/build-abi-js.sh
+++ scripts/build-abi-js.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")/.."
 
-which jq &>/dev/null || { echo "Install jq utility!" && exit; }
+which jq &>/dev/null || { echo "Install jq utility!" && exit 1; }
 
 CONTRACTS=(
     IERC20
@@ -16,9 +16,11 @@ CONTRACTS=(
     IInstantDistributionAgreementV1
 )
 
-echo "if (typeof module === \"undefined\") module = {};"
-echo "Superfluid_ABI = module.exports = {"
-for i in "${CONTRACTS[@]}";do
-    echo "    $i: $(jq -c '.abi' build/contracts/$i.json),"
-done
-echo "};"
+{
+    echo "if (typeof module === \"undefined\") module = {};"
+    echo "Superfluid_ABI = module.exports = {"
+    for i in "${CONTRACTS[@]}";do
+        echo "    $i: $(jq -c '.abi' build/contracts/$i.json),"
+    done
+    echo "};"
+} > build/abi.js

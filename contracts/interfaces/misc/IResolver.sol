92e5de0cedf446ad2e89adba2b2fdeee58558d9b
Miao ZhiCheng <miao@decentral.ee>
2020-09-30 20:16:52 +0300

    WIP for later

--- contracts/interfaces/misc/IResolver.sol
+++ contracts/interfaces/misc/IResolver.sol
@@ -9,11 +9,6 @@ pragma solidity >= 0.5.0;
 interface IResolver {
 
     /**
-     * @dev Set name to the target.
-     */
-    function set(string calldata name, address target) external;
-
-    /**
      * @dev Get address by name.
      */
     function get(string calldata name) external view returns (address);

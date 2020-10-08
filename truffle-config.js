1677dc63b997ffaddf22603c0a482f33ba879c0d
Miao ZhiCheng <miao@decentral.ee>
2020-10-08 16:19:39 +0300

    make GOERLI_GAS_PRICE configurable

--- truffle-config.js
+++ truffle-config.js
@@ -78,7 +78,7 @@ module.exports = {
             ),
             network_id: 5, // Goerli's id
             gas: GAS_LIMIT,
-            gasPrice: 10e9, // 10 GWEI
+            gasPrice: +process.env.GOERLI_GAS_PRICE || 10e9, // 10 GWEI
             //confirmations: 6, // # of confs to wait between deployments. (default: 0)
             timeoutBlocks: 50, // # of blocks before a deployment times out  (minimum/default: 50)
             skipDryRun: false // Skip dry run before migrations? (default: false for public nets )
@@ -95,7 +95,7 @@ module.exports = {
             network_id: 1, // mainnet's id
             //gas: 7017622, //
             gas: GAS_LIMIT,
-            gasPrice: +process.env.MAINNET_GAS_PRICE || 1000*1000*1000, // default 1 gwei
+            gasPrice: +process.env.MAINNET_GAS_PRICE || 1e9, // default 1 gwei
             //confirmations: 6, // # of confs to wait between deployments. (default: 0)
             timeoutBlocks: 50, // # of blocks before a deployment times out  (minimum/default: 50)
             skipDryRun: false // Skip dry run before migrations? (default: false for public nets )

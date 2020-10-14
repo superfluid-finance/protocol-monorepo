8083f9c8531b2edd57a5be8fd755ad285b54a232
Miao ZhiCheng <hellwolf.misty@gmail.com>
2020-10-14 12:58:10 +0300

    Resolver access control (#103)
    - set new resolver for goerli testnet

--- contracts/test/TestResolver.sol
+++ contracts/test/TestResolver.sol
@@ -2,14 +2,32 @@
 /* solhint-disable not-rely-on-time */
 pragma solidity 0.7.1;
 
+import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
 import { IResolver } from "../interfaces/misc/IResolver.sol";
 
 
-contract TestResolver is IResolver {
+contract TestResolver is IResolver, AccessControl {
+
+    bytes32 public constant RESOLVER_ADMIN_ROLE = keccak256("RESOLVER_ADMIN_ROLE");
 
     mapping(string => address) private _registry;
 
+    constructor() {
+        _setupRole(RESOLVER_ADMIN_ROLE, msg.sender);
+    }
+
+    function isAdmin(address account) external view returns (bool) {
+        return hasRole(RESOLVER_ADMIN_ROLE, account);
+    }
+
+    function grantAdmin(address account) external {
+        require(hasRole(RESOLVER_ADMIN_ROLE, _msgSender()), "AccessControl: sender must be an admin to grant");
+
+        _setupRole(RESOLVER_ADMIN_ROLE, account);
+    }
+
     function set(string calldata name, address target) external {
+        require(hasRole(RESOLVER_ADMIN_ROLE, _msgSender()), "Caller is not an admin");
         _registry[name] = target;
     }
 

5653e837cb9c701e88cd1c921ffe078a40449894
Miao ZhiCheng <miao@superfluid.finance>
2020-10-20 22:00:53 +0300

    rename the test

--- test/contracts.test.js
+++ test/contracts.test.js
@@ -1,5 +1,5 @@
 require("./superfluid/Superfluid.test.js");
 require("./superfluid/SuperToken.test.js");
 require("./agreements/ConstantFlowAgreementV1.test.js");
-require("./agreements/InstantDistributionAgreementV1.test.js");
+require("./contracts/agreements/InstantDistributionAgreementV1.test.js");
 require("./apps/MultiFlowsApp.test.js");

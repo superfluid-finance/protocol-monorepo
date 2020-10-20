ea5b3a2362f78c88445809a54a0ec513993151c0
Miao ZhiCheng <hellwolf.misty@gmail.com>
2020-10-20 12:45:50 +0300

    Framework class + ConstantFlowAgreementV1Helper class (#110)
    - Framework class supports token list preloading
    - ConstantFlowAgreementV1Helper

---
+++ js-sdk/InstantDistributionAgreementV1Helper.js
@@ -0,0 +1,8 @@
+/**
+ * @dev Instant distribution agreement v1 helper class
+ */
+module.exports = class InstantDistributionAgreementV1Helper {
+    constructor(sf) {
+        this._sf = sf;
+    }
+};
--- truffle-config.js
+++ truffle-config.js
@@ -98,6 +98,22 @@ module.exports = {
             skipDryRun: false // Skip dry run before migrations? (default: false for public nets )
         },
 
+        mumbai: {
+            provider: () => new HDWalletProvider(
+                process.env.MUMBAI_MNEMONIC,
+                process.env.MUMBAI_PROVIDER_URL,
+                0, //address_index
+                10, // num_addresses
+                true // shareNonce
+            ),
+            network_id:  80001, // mainnet's id
+            gas: 8e6,
+            gasPrice: +process.env.MUMBAI_GAS_PRICE || 1e9, // default 1 gwei
+            //confirmations: 6, // # of confs to wait between deployments. (default: 0)
+            timeoutBlocks: 50, // # of blocks before a deployment times out  (minimum/default: 50)
+            skipDryRun: false // Skip dry run before migrations? (default: false for public nets )
+        },
+
         coverage: {
             host: "localhost",
             network_id: "*",

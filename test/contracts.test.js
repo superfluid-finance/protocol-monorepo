60bba14ec6eff29030176ef6c479b10e794e6983
Miao ZhiCheng <miao@superfluid.finance>
2020-10-22 10:05:59 +0300

    include DRT tokent test back

--- test/contracts.test.js
+++ test/contracts.test.js
@@ -4,3 +4,4 @@ require("./contracts/agreements/ConstantFlowAgreementV1.test.js");
 require("./contracts/agreements/InstantDistributionAgreementV1.test.js");
 
 require("./apps/MultiFlowsApp.test.js");
+require("./apps/DividendRightsToken.test.js");

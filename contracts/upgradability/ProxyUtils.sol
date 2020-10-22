188c3c2613b10493f808da1f09c86456561e29b3
Miao ZhiCheng <miao@superfluid.finance>
2020-10-22 10:09:12 +0300

    remove more dead code

--- contracts/upgradability/ProxyUtils.sol
+++ contracts/upgradability/ProxyUtils.sol
@@ -13,11 +13,6 @@ library ProxyUtils {
      */
     bytes32 internal constant _IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
 
-    /// @dev Get implementation slot value.
-    function implementationSlot() internal pure returns (bytes32 slot) {
-        return _IMPLEMENTATION_SLOT;
-    }
-
     /// @dev Get implementation address.
     function implementation() internal view returns (address impl) {
         assembly { // solium-disable-line

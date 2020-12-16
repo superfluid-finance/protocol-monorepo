d472d87ffd4ea9a4e0721c9b943f70a0ac89d541
Miao ZhiCheng <miao@superfluid.finance>
2020-12-16 18:44:00 +0200

    fix build

--- js-sdk/getConfig.js
+++ js-sdk/getConfig.js
@@ -3,7 +3,7 @@ const DEFAULT_CONFIG = {
         resolverAddress: "0x3710AB3fDE2B61736B8BB0CE845D6c61F667a78E"
     },
     80001: {
-        resolverAddress: "0x3710AB3fDE2B61736B8BB0CE845D6c61F667a78E"
+        resolverAddress: "0x8C54C83FbDe3C59e59dd6E324531FB93d4F504d3"
     }
 };
 

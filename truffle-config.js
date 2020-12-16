1604a671c25fb73f2462a0f4b15a0064ff57da97
Miao ZhiCheng <miao@superfluid.finance>
2020-12-16 18:19:33 +0200

    mumbai or mombasa

--- truffle-config.js
+++ truffle-config.js
@@ -76,7 +76,7 @@ module.exports = {
             ),
             network_id: 5, // Goerli's id
             gas: 8e6,
-            gasPrice: +process.env.GOERLI_GAS_PRICE || 10e9, // 10 GWEI
+            gasPrice: +process.env.GOERLI_GAS_PRICE || 100e9, // 100 GWEI, goerli is busy!
             //confirmations: 6, // # of confs to wait between deployments. (default: 0)
             timeoutBlocks: 50, // # of blocks before a deployment times out  (minimum/default: 50)
             skipDryRun: false // Skip dry run before migrations? (default: false for public nets )

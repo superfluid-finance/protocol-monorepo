65212422a3859ed36b895197ddcaff2be469bdf3
Miao ZhiCheng <miao@superfluid.finance>
2020-12-30 12:22:47 +0200

    fix

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
         "describe": true,
         "contract": true,
         "context": true,
-        "after": true,
         "before": true,
         "after": true,
         "beforeEach": true,

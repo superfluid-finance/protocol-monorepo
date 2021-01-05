9aa5ca1787bad7b05ea3952f594bcf438ac275ec
Patrick <blockchainbuddha@gmail.com>
2021-01-05 16:28:12 -0500

    scaffold user test

--- js-sdk/User.js
+++ js-sdk/User.js
@@ -1,6 +1,6 @@
 const { getErrorResponse } = require("./utils/error");
 
-export default class User {
+module.exports = class User {
     constructor({ sf, address, token, options }) {
         this.sf = sf;
         this.address = address;
@@ -9,26 +9,45 @@ export default class User {
     }
 
     async details() {
-        const flows = await listFlows({
-            superToken: this.token,
-            account: this.address
-        });
-        const netFlow = await getNetFlow({ superToken: this.token, account: this.address });
-        return { cfa: { flows, netFlow } };
-    }
-    async sendFlow(recipient, flowRate) {
         try {
-            // await async createFlow({
-            //     superToken,
-            //     sender,
-            //     receiver,
-            //     flowRate,
-            //     userData,
-            //     onTransaction = () => null,
-            // })
-            // return interestSent;
-        } catch (error) {
-            throw getErrorResponse(error, "user", "interestSent");
+            const flows = await listFlows({
+                superToken: this.token,
+                account: this.address
+            });
+            const netFlow = await getNetFlow({ superToken: this.token, account: this.address });
+            return { cfa: { flows, netFlow } };
+        } catch (e) {
+            throw getErrorResponse(e, "user", "details");
         }
     }
-}
+
+    async flow(recipient, flowRate) {
+        try {
+            if (flowRate === 0) {
+                await this.cfa.deleteFlow({});
+                return;
+            }
+            const existingFlow = await getFlow({ superToken: this.token, sender: this.address, receiver: recipient });
+            if (existingFlow > 0) {
+                await this.cfa.updateFlow({});
+                return;
+            }
+            await this.cfa.createFlow({
+                superToken: this.token,
+                sender: this.address,
+                receiver: recipient,
+                flowRate
+                // userData,
+                // onTransaction = () => null,
+            });
+        } catch (e) {
+            throw getErrorResponse(e, "user", "createFlow");
+        }
+    }
+    async getFlow(recipient) {
+        try {
+        } catch (e) {
+            throw getErrorResponse(e, "user", "getFlow");
+        }
+    }
+};
--- js-sdk/utils/general.js
+++ js-sdk/utils/general.js
@@ -1,14 +1,14 @@
-const { getErrorResponse } = require("./error")
-
-const validateAddress = (address) => {
-  if (!isAddress(address)) throw "Address is invalid"
+const validateAddress = address => {
+    if (!isAddress(address)) throw "Address is invalid";
 };
 
-const isAddress = (address) => {
-  return /^(0x)?[0-9a-f]{40}$/i.test(address);
+const isAddress = address => {
+    return /^(0x)?[0-9a-f]{40}$/i.test(address);
 };
 
-export const getCleanAddress = (address) => {
-  validateAddress(address);
-  return address.toLowerCase();
+const getCleanAddress = address => {
+    validateAddress(address);
+    return address.toLowerCase();
 };
+
+module.exports = { validateAddress, isAddress, getCleanAddress };

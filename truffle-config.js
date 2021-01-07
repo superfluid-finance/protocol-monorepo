16bc3518e82fe02c9f9900335965ecfd97b7e62a
Miao ZhiCheng <miao@superfluid.finance>
2021-01-07 19:49:29 +0200

    some frivilous commit

--- truffle-config.js
+++ truffle-config.js
@@ -98,22 +98,6 @@ module.exports = {
             skipDryRun: false // Skip dry run before migrations? (default: false for public nets )
         },
 
-        mainnet: {
-            provider: () => new HDWalletProvider(
-                process.env.MAINNET_MNEMONIC,
-                process.env.MAINNET_PROVIDER_URL,
-                0, //address_index
-                10, // num_addresses
-                true // shareNonce
-            ),
-            network_id: 1, // mainnet's id
-            gas: 8e6,
-            gasPrice: +process.env.MAINNET_GAS_PRICE || 1e9, // default 1 gwei
-            //confirmations: 6, // # of confs to wait between deployments. (default: 0)
-            timeoutBlocks: 50, // # of blocks before a deployment times out  (minimum/default: 50)
-            skipDryRun: false // Skip dry run before migrations? (default: false for public nets )
-        },
-
         mumbai: {
             provider: () => new HDWalletProvider(
                 process.env.MUMBAI_MNEMONIC,
@@ -141,6 +125,22 @@ module.exports = {
             network_id: 0x03C401, // artis tau1 network
             gas: 8e6,
             gasPrice: +process.env.ARTIS_GAS_PRICE || 1e9, // default 1 gwei
+            //confirmations: 6, // # of confs to wait between deployments. (default: 0)
+            timeoutBlocks: 50, // # of blocks before a deployment times out  (minimum/default: 50)
+            skipDryRun: false // Skip dry run before migrations? (default: false for public nets )
+        },
+
+        mainnet: {
+            provider: () => new HDWalletProvider(
+                process.env.MAINNET_MNEMONIC,
+                process.env.MAINNET_PROVIDER_URL,
+                0, //address_index
+                10, // num_addresses
+                true // shareNonce
+            ),
+            network_id: 1, // mainnet's id
+            gas: 8e6,
+            gasPrice: +process.env.MAINNET_GAS_PRICE || 1e9, // default 1 gwei
             //confirmations: 6, // # of confs to wait between deployments. (default: 0)
             timeoutBlocks: 50, // # of blocks before a deployment times out  (minimum/default: 50)
             skipDryRun: false // Skip dry run before migrations? (default: false for public nets )

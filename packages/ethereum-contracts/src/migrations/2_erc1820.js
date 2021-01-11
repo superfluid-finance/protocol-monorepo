1844c592ac4b71199a864b2830ca52edcf984ac8
Miao ZhiCheng <miao@superfluid.finance>
2021-01-11 14:14:22 +0200

    fix the build

--- packages/ethereum-contracts/src/migrations/2_erc1820.js
+++ packages/ethereum-contracts/src/migrations/2_erc1820.js
@@ -1,5 +1,5 @@
 const Web3 = require("web3");
-const deploy = require("../scripts/deploy-erc1820");
+const deploy = require("../../scripts/deploy-erc1820");
 
 module.exports = async function(deployer) {
     const errorHandler = err => {
--- packages/ethereum-contracts/truffle-config.js
+++ packages/ethereum-contracts/truffle-config.js
@@ -32,6 +32,7 @@ module.exports = {
         "truffle-plugin-verify"
     ],
     contracts_directory: "./src/contracts",
+    migrations_directory: "./src/migrations",
     /**
      * Networks define how you connect to your ethereum client and let you set the
      * defaults web3 uses to send transactions. If you don't specify one truffle

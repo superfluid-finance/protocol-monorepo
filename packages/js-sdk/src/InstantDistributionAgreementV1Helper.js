dc20241cdcfffb7d2ac74323f45abe6ddc92f8b5
patrick <blockchainbuddha@gmail.com>
2021-01-13 21:40:53 -0500

    updateSupscription and approveSupscription

--- packages/js-sdk/src/InstantDistributionAgreementV1Helper.js
+++ packages/js-sdk/src/InstantDistributionAgreementV1Helper.js
@@ -52,7 +52,89 @@ module.exports = class InstantDistributionAgreementV1Helper {
                 { from: sender }
             )
             .on("transactionHash", onTransaction);
-        console.debug("Index created.");
+        console.debug("Index updated.");
+        return tx;
+    }
+
+    async updateSupscription({
+        superToken,
+        indexId,
+        subscriber,
+        sender,
+        units,
+        userData = "0x",
+        onTransaction = () => null
+    }) {
+        const tx = await this._sf.host
+            .callAgreement(
+                this._ida.address,
+                this._ida.contract.methods
+                    .updateSubscription(
+                        superToken,
+                        indexId,
+                        subscriber,
+                        units,
+                        "0x"
+                    )
+                    .encodeABI(),
+                userData,
+                { from: sender }
+            )
+            .on("transactionHash", onTransaction);
+        console.debug("Subscription updated.");
+        return tx;
+    }
+
+    async approveSupscription({
+        superToken,
+        indexId,
+        publisher,
+        sender,
+        units,
+        userData = "0x",
+        onTransaction = () => null
+    }) {
+        const tx = await this._sf.host
+            .callAgreement(
+                this._ida.address,
+                this._ida.contract.methods
+                    .approveSubscription(superToken, publisher, indexId, "0x")
+                    .encodeABI(),
+                userData,
+                { from: sender }
+            )
+            .on("transactionHash", onTransaction);
+        console.debug("Subscription approved.");
+        return tx;
+    }
+
+    async deleteSupscription({
+        superToken,
+        indexId,
+        publisher,
+        subscriber,
+        sender,
+        units,
+        userData = "0x",
+        onTransaction = () => null
+    }) {
+        const tx = await this._sf.host
+            .callAgreement(
+                this._ida.address,
+                this._ida.contract.methods
+                    .deleteSubscription(
+                        superToken,
+                        publisher,
+                        indexId,
+                        subscriber,
+                        "0x"
+                    )
+                    .encodeABI(),
+                userData,
+                { from: sender }
+            )
+            .on("transactionHash", onTransaction);
+        console.debug("Subscription deleted.");
         return tx;
     }
 };
--- packages/js-sdk/test/InstantDistributionAgreementV1Helper.test.js
+++ packages/js-sdk/test/InstantDistributionAgreementV1Helper.test.js
@@ -58,4 +58,87 @@ contract("InstantDistributionAgreementV1Helper helper class", accounts => {
         );
         assert.equal(index.indexValue, indexValue);
     });
+
+    describe("subscriptions", () => {
+        const indexId = 1;
+        const subscriber = bob;
+        const publisher = alice;
+        beforeEach(async () => {
+            await sf.ida.createIndex({
+                superToken: superToken.address,
+                indexId,
+                sender: publisher
+            });
+        });
+
+        it("updateSupscription", async () => {
+            const units = 100;
+            await sf.ida.updateSupscription({
+                superToken: superToken.address,
+                indexId,
+                subscriber,
+                units,
+                sender: publisher
+            });
+
+            const index = await sf.agreements.ida.getIndex(
+                superToken.address,
+                alice,
+                indexId
+            );
+            assert.equal(index.totalUnitsPending, units);
+        });
+
+        it("approveSupscription", async () => {
+            const units = 100;
+            await sf.ida.updateSupscription({
+                superToken: superToken.address,
+                indexId,
+                subscriber,
+                units,
+                sender: publisher
+            });
+
+            await sf.ida.approveSupscription({
+                superToken: superToken.address,
+                indexId,
+                publisher,
+                sender: subscriber
+            });
+
+            const index = await sf.agreements.ida.getIndex(
+                superToken.address,
+                alice,
+                indexId
+            );
+            console.log(index.totalUnitsApproved === units);
+            assert.equal(index.totalUnitsApproved, units);
+        });
+
+        it("deleteSupscription", async () => {
+            const units = 100;
+            await sf.ida.updateSupscription({
+                superToken: superToken.address,
+                indexId,
+                subscriber,
+                units,
+                sender: publisher
+            });
+
+            await sf.ida.deleteSupscription({
+                superToken: superToken.address,
+                indexId,
+                publisher,
+                sender: subscriber
+            });
+
+            const index = await sf.agreements.ida.getIndex(
+                superToken.address,
+                alice,
+                indexId
+            );
+            console.log(JSON.stringify(index));
+            assert.equal(index.totalUnitsApproved, units);
+        });
+    });
 });

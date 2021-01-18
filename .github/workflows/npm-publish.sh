b9124d935cdd745bb2b14f8a4b915c25ed51c564
Miao ZhiCheng <miao@superfluid.finance>
2021-01-19 00:04:11 +0200

    new publish master flow

--- .github/workflows/npm-publish.sh
+++ .github/workflows/npm-publish.sh
@@ -7,10 +7,12 @@ git diff --quiet || { echo "There are unstaged local changes"; exit 1; }
 git diff --cached --quiet || { echo "There are staged local changes"; exit 1; }
 
 VERSION=`awk -F'"' '/"version": ".+"/{ print $4; exit; }' package.json`
-TIME=`date -u +%Y%m%dT%H%M%SZ`
-GIT_REV=`git rev-parse --short HEAD`
 
+# if version is ended with -latest, replace it with an unique latest-TIME-GIT_REV
 if echo "$VERSION" | fgrep -q -- "-latest";then
+    TIME=`date -u +%Y%m%dT%H%M%SZ`
+    GIT_REV=`git rev-parse --short HEAD`
+
     PUBLISHING_VERSION=${VERSION%-latest*}-latest-${TIME}-${GIT_REV}
 
     jq -r ". |= . + {
@@ -23,6 +25,17 @@ fi
 
 echo "Publishing pacakge, version $PUBLISHING_VERSION"
 
-npm publish --access public
+# publish to the private github registry
+cat > .npmrc <<EOF
+@superfluid-finance:registry=https://npm.pkg.github.com
+//npm.pkg.github.com/:_authToken=${GITHUB_TOKEN}
+always-auth=true
+EOF
+npm publish --access public --dry-run
 
+# publish to the npmjs registry
+rm -f .npmrc
+npm publish --access public --dry-run
+
+# revert the changes to the package.json if any
 git checkout package.json

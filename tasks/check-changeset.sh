15e074272211680236e62910cf978191c8963cc2
Miao ZhiCheng <miao@superfluid.finance>
2021-01-21 16:39:04 +0200

    fix check-changeset

--- tasks/check-changeset.sh
+++ tasks/check-changeset.sh
@@ -4,9 +4,9 @@ SOURCE_REF=$1
 BASE_REF=$2
 
 # fetch the latest commit of the base ref
-git fetch --depth=1 origin ${BASE_REF}:${BASE_REF}
+git fetch --depth=1 origin refs/heads/${BASE_REF}:refs/remotes/origin/${BASE_REF}
 # compare the source branch with the dev branch
-git diff --name-only ${SOURCE_REF} ${BASE_REF} > changed-files.list
+git diff --name-only ${SOURCE_REF} refs/remotes/origin/${BASE_REF} > changed-files.list
 echo Changed files:
 echo ---
 cat changed-files.list

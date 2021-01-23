2796f5959eb03d5a7ec4cfa583cbed1123db6b71
Miao ZhiCheng <miao@superfluid.finance>
2021-01-23 02:39:25 +0200

    actually publish pr

--- tasks/publish-pr.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-# This publishes a "PR" version of our packages to Github.
-# It is run when a new PR is made.
-# https://github.com/lerna/lerna/tree/master/commands/publish#--canary
-
-yarn lerna publish --canary --preid "PR${PR_NUMBER}.${SHORT_REV}" --dist-tag PR${PR_NUMBER} --registry "https://npm.pkg.github.com" --yes

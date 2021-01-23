4411922fb1b4972c548e8d11429a257c2ffd6d94
Miao ZhiCheng <hellwolf.misty@gmail.com>
2021-01-23 02:22:45 +0200

    Process 176 setup branching policy (#196)
    Co-authored-by: patrick <blockchainbuddha@gmail.com>

---
+++ tasks/publish-canary.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+
+# This publishes a "canary" version of our packages to npm.
+# It is run when a branch is merged to dev.
+# https://github.com/lerna/lerna/tree/master/commands/publish#--canary
+
+yarn lerna publish --canary --preid dev --dist-tag dev --yes

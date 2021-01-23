207490ff85b3703d0be0d5b7eb8e722bd5b15b0b
Miao ZhiCheng <miao@superfluid.finance>
2021-01-23 14:14:34 +0200

    fixes.

--- tasks/npm-publish.sh
+++ tasks/npm-publish.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
 echo "Publishing $1 to NPMJS registry"
-./tasks/npm-use-npmjs.sh > .npmrc
+./npmrc-use-npmjs.sh > .npmrc
 npm publish --access public --dry-run $1
 
 echo "Publishing $1 to Github Packages"
-./tasks/npm-use-github.sh > .npmrc
+./npmrc-use-github.sh > .npmrc
 npm publish --access public --dry-run $1

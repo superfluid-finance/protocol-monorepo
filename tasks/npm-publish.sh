ed1d995d88852544e8f4de745f5e4db062d80b91
Miao ZhiCheng <miao@superfluid.finance>
2021-01-23 14:57:58 +0200

    remove dry-run

--- tasks/npm-publish.sh
+++ tasks/npm-publish.sh
@@ -2,10 +2,10 @@
 
 D="$(dirname "$0")"
 
-echo "Publishing $1 to NPMJS registry"
+echo "Publishing $1 @$2 to NPMJS registry"
 $D/npmrc-use-npmjs.sh > .npmrc
-npm publish --access public --dry-run $1
+npm publish --tag $2 $1
 
-echo "Publishing $1 to Github Packages"
+echo "Publishing $1 @$2 to Github Packages"
 $D/npmrc-use-github.sh > .npmrc
-npm publish --access public --dry-run $1
+npm publish --tag $2 $1

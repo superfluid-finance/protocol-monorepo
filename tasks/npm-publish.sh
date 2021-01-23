59436b55eae86f6603f9daae065a9a4a674bafd3
Miao ZhiCheng <miao@superfluid.finance>
2021-01-23 14:49:54 +0200

    use .shortRev in versions

--- tasks/npm-publish.sh
+++ tasks/npm-publish.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
 
+D="$(dirname "$0")"
+
 echo "Publishing $1 to NPMJS registry"
-./npmrc-use-npmjs.sh > .npmrc
+$D/npmrc-use-npmjs.sh > .npmrc
 npm publish --access public --dry-run $1
 
 echo "Publishing $1 to Github Packages"
-./npmrc-use-github.sh > .npmrc
+$D/npmrc-use-github.sh > .npmrc
 npm publish --access public --dry-run $1

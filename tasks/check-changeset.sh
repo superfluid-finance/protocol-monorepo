4a2202a2b9a55cd029a678532154e09a445aca27
Miao ZhiCheng <miao@superfluid.finance>
2021-02-10 01:01:47 +0200

    debug

--- tasks/check-changeset.sh
+++ tasks/check-changeset.sh
@@ -8,9 +8,12 @@ echo BASE_REF: $BASE_REF
 
 # fetch the latest commit of the base ref
 git fetch --depth=1 origin refs/heads/${BASE_REF}:refs/remotes/origin/${BASE_REF}
+
 # compare the source branch with the dev branch
+git merge-base ${SOURCE_REF} refs/remotes/origin/${BASE_REF}
 MERGE_BASE_REF=$(git merge-base ${SOURCE_REF} refs/remotes/origin/${BASE_REF})
 echo MERGE_BASE_REF: $MERGE_BASE_REF
+
 git diff --name-only ${SOURCE_REF} ${MERGE_BASE_REF} > changed-files.list
 echo Changed files:
 echo ---

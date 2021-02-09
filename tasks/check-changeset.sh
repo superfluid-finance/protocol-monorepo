0cb833d20be66a09017ba05533e0ec0f9bb20c1c
Miao ZhiCheng <miao@superfluid.finance>
2021-02-10 00:50:02 +0200

    use merge-base in check-changeset.sh

--- tasks/check-changeset.sh
+++ tasks/check-changeset.sh
@@ -6,7 +6,8 @@ BASE_REF=$2
 # fetch the latest commit of the base ref
 git fetch --depth=1 origin refs/heads/${BASE_REF}:refs/remotes/origin/${BASE_REF}
 # compare the source branch with the dev branch
-git diff --name-only ${SOURCE_REF} refs/remotes/origin/${BASE_REF} > changed-files.list
+MERGE_BASE_REF=$(git merge-base ${SOURCE_REF} refs/remotes/origin/${BASE_REF})
+git diff --name-only ${SOURCE_REF} ${MERGE_BASE_REF} > changed-files.list
 echo Changed files:
 echo ---
 cat changed-files.list

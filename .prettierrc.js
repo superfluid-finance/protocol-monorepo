546fd0c391fe26869db517469762d35a7afd5714
Miao ZhiCheng <miao@superfluid.finance>
2021-02-12 14:55:33 +0200

    fix

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,4 @@
 module.exports = {
+    trailingComma: "none",
     singleQuote: false
 };
--- packages/js-sdk/src/Framework.js
+++ packages/js-sdk/src/Framework.js
@@ -95,7 +95,7 @@ module.exports = class Framework {
             ethers: this._options.ethers,
             from: this._options.from,
             additionalContracts: this._options.additionalContracts,
-            contractLoader: this._options.contractLoader,
+            contractLoader: this._options.contractLoader
         });
 
         const resolverAddress =
@@ -126,7 +126,7 @@ module.exports = class Framework {
             cfa: await this.contracts.IConstantFlowAgreementV1.at(cfaAddress),
             ida: await this.contracts.IInstantDistributionAgreementV1.at(
                 idaAddress
-            ),
+            )
         };
 
         // load agreement helpers
@@ -183,9 +183,9 @@ module.exports = class Framework {
             if (tokenAddress === ZERO_ADDRESS) {
                 throw new Error(`Token ${tokenSymbol} is not registered`);
             }
-            this.tokens[tokenSymbol] = await this.contracts.IERC20.at(
-                tokenAddress
-            );
+            this.tokens[
+                tokenSymbol
+            ] = await this.contracts.ERC20WithTokenInfo.at(tokenAddress);
             console.debug(
                 `${tokenSymbol}: ERC20WithTokenInfo .tokens["${tokenSymbol}"] @${tokenAddress}`
             );

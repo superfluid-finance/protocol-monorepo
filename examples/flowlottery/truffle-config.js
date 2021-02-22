702cf65ef17bcd9c239504d57184453e19fae39b
Miao ZhiCheng <miao@superfluid.finance>
2021-02-22 17:01:35 +0200

    randomize GANACHE_PORT

--- examples/flowlottery/truffle-config.js
+++ examples/flowlottery/truffle-config.js
@@ -20,7 +20,7 @@ module.exports = {
         ganache: {
             host: "127.0.0.1",
             network_id: "*",
-            port: 8545
+            port: process.env.GANACHE_PORT || 8545,
         }
     },
     compilers: {

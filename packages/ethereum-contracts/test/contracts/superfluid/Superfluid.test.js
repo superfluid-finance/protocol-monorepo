400fa7cab7300af679d9117b704e3eb8b520e738
Miao ZhiCheng <miao@superfluid.finance>
2021-02-25 17:47:01 +0200

    fixed builds

--- packages/ethereum-contracts/test/contracts/superfluid/Superfluid.test.js
+++ packages/ethereum-contracts/test/contracts/superfluid/Superfluid.test.js
@@ -2022,9 +2022,14 @@ contract("Superfluid Host Contract", (accounts) => {
             });
 
             it("#11.1 forwardBatchCall with mocked transaction signer", async () => {
-                await governance.enableTrustedForwarder(forwarder.address, {
-                    from: admin,
-                });
+                await governance.enableTrustedForwarder(
+                    superfluid.address,
+                    ZERO_ADDRESS,
+                    forwarder.address,
+                    {
+                        from: admin,
+                    }
+                );
                 await t.createNewToken({ doUpgrade: false });
                 const { superToken } = t.contracts;
                 await t.upgradeBalance("alice", toWad(1));
@@ -2056,9 +2061,14 @@ contract("Superfluid Host Contract", (accounts) => {
             });
 
             it("#11.2 untrusted forwarder", async () => {
-                await governance.disableTrustedForwarder(forwarder.address, {
-                    from: admin,
-                });
+                await governance.disableTrustedForwarder(
+                    superfluid.address,
+                    ZERO_ADDRESS,
+                    forwarder.address,
+                    {
+                        from: admin,
+                    }
+                );
                 await expectRevert(
                     web3tx(forwarder.execute, "forwarder.execute")(
                         {

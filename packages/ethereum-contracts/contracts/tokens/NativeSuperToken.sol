080c96b66aaf1cefe3b3b87887033275c8087691
didi <git@d10r.net>
2021-03-15 10:28:20 +0100

    added custom token and deploy script for a native SuperToken

---
+++ packages/ethereum-contracts/contracts/tokens/NativeSuperToken.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: AGPLv3
+pragma solidity 0.7.6;
+
+import {
+    ISuperToken,
+    CustomSuperTokenProxyBase
+}
+from "../interfaces/superfluid/CustomSuperTokenProxyBase.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { UUPSProxy } from "../upgradability/UUPSProxy.sol";
+
+/**
+ * @dev Native SuperToken custom token functions
+ *
+ * @author Superfluid
+ */
+interface INativeSuperTokenCustom {
+    function initialize(string calldata name, string calldata symbol, uint256 initialSupply) external;
+}
+
+/**
+ * @dev Native SuperToken full interface
+ *
+ * @author Superfluid
+ */
+interface INativeSuperToken is INativeSuperTokenCustom, ISuperToken {
+    function initialize(string calldata name, string calldata symbol, uint256 initialSupply) external override;
+}
+
+/**
+ * @dev Native SuperToken custom super token implementation
+ *
+ * @author Superfluid
+ */
+contract NativeSuperTokenProxy is INativeSuperTokenCustom, CustomSuperTokenProxyBase {
+    function initialize(string calldata name, string calldata symbol, uint256 initialSupply)
+        external override
+    {
+        ISuperToken(address(this)).initialize(
+            IERC20(0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF), // 0xFF... for "no underlying token"
+            18, // not sure if this has any effect in our case
+            name,
+            symbol
+        );
+        ISuperToken(address(this)).selfMint(msg.sender, initialSupply, new bytes(0));
+    }
+}

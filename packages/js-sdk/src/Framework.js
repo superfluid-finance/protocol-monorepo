5ad41322742e9bf9018a0ecf32c434f4b2421a64
msoni89 <65469737+msoni89@users.noreply.github.com>
2021-04-08 16:11:09 +0300

    loadToken of superTokenKey can load underlyingToken too (#311)
    * loadToken of superTokenKey can load underlyingToken too 
    * added isSuprTokenListed
    
    Co-authored-by: Mehul Soni <mehul@superfluid.com>
    Co-authored-by: Miao ZhiCheng <miao@superfluid.finance>

--- packages/js-sdk/src/Framework.js
+++ packages/js-sdk/src/Framework.js
@@ -182,6 +182,17 @@ module.exports = class Framework {
 
     /**
      * @dev Load additional token using resolver
+     * @param {String} superTokenKey super token key used to query resolver
+     */
+    async isSuprTokenListed(superTokenKey) {
+        const superTokenAddress = await this.resolver.get(
+            `supertokens.${this.version}.${superTokenKey}`
+        );
+        return superTokenAddress !== ZERO_ADDRESS;
+    }
+
+    /**
+     * @dev Load additional token using resolver
      * @param {String} tokenKey token key used to query resolver (in order of preference):
      *    - super chain-native token symbol (see getConfig.js),
      *    - underlying token resolver key (tokens.{KEY}),
@@ -191,6 +202,7 @@ module.exports = class Framework {
         // load underlying token
         //  but we don't need to load native tokens
         let underlyingToken;
+        let checkUnderlyingToken;
         let superTokenKey;
         if (tokenKey !== this.config.nativeTokenSymbol) {
             const tokenAddress = await this.resolver.get(`tokens.${tokenKey}`);
@@ -207,6 +219,7 @@ module.exports = class Framework {
             } else {
                 superTokenKey = tokenKey;
             }
+            checkUnderlyingToken = true;
         } else {
             superTokenKey = this.config.nativeTokenSymbol + "x";
         }
@@ -226,7 +239,39 @@ module.exports = class Framework {
         }
         this.tokens[superTokenKey] = superToken;
         this.superTokens[superTokenKey] = superToken;
+
+        if (checkUnderlyingToken) {
+            const underlyingTokenAddress = await superToken.getUnderlyingToken.call();
+            if (underlyingTokenAddress !== ZERO_ADDRESS) {
+                // if underlying token is not undefined and not equal to getUnderlyingToken() returned address
+                if (
+                    underlyingToken &&
+                    underlyingTokenAddress.toLowerCase() !==
+                        underlyingToken.address.toLowerCase()
+                ) {
+                    throw new Error(
+                        `Underlying token address are different for ${tokenKey}`
+                    );
+                }
+
+                // if underlying token is null or undefined
+                if (!underlyingToken) {
+                    underlyingToken = await this.contracts.ERC20WithTokenInfo.at(
+                        underlyingTokenAddress
+                    );
+                    const symbol = await underlyingToken.symbol();
+                    this.tokens[symbol] = underlyingToken;
+                }
+            } else {
+                if (underlyingToken) {
+                    throw new Error(
+                        `Unexpected underlying token for ${tokenKey}`
+                    );
+                }
+            }
+        }
         superToken.underlyingToken = underlyingToken;
+
         console.debug(
             `${superTokenKey}: ISuperToken .tokens["${superTokenKey}"]`,
             superTokenAddress

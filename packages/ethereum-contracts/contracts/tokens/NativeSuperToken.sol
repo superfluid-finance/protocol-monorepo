af486fa1508c5f65e8079d38327b95f99e90b030
Didi <dho@lab10.coop>
2021-04-08 17:07:57 +0200

    Minimal native SuperToken with deploy script (#306)
    * added custom token proxy and deploy script for a native SuperToken
    * add NativeSuperTokenProxy.test.js
    
    Co-authored-by: Miao ZhiCheng <miao@superfluid.finance>

---
+++ packages/ethereum-contracts/contracts/tokens/NativeSuperToken.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: AGPLv3
+pragma solidity 0.7.6;
+
+import {
+    ISuperToken,
+    CustomSuperTokenProxyBase
+}
+from "../interfaces/superfluid/CustomSuperTokenProxyBase.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { UUPSProxy } from "../upgradability/UUPSProxy.sol";
+
+/**
+ * @dev Native SuperToken custom token functions
+ *
+ * @author Superfluid
+ */
+interface INativeSuperTokenCustom {
+    function initialize(string calldata name, string calldata symbol, uint256 initialSupply) external;
+}
+
+/**
+ * @dev Native SuperToken full interface
+ *
+ * @author Superfluid
+ */
+interface INativeSuperToken is INativeSuperTokenCustom, ISuperToken {
+    function initialize(string calldata name, string calldata symbol, uint256 initialSupply) external override;
+}
+
+/**
+ * @dev Native SuperToken custom super token implementation
+ *
+ * NOTE:
+ * - This is a simple implementation where the supply is pre-minted.
+ *
+ * @author Superfluid
+ */
+contract NativeSuperTokenProxy is INativeSuperTokenCustom, CustomSuperTokenProxyBase {
+    function initialize(string calldata name, string calldata symbol, uint256 initialSupply)
+        external override
+    {
+        ISuperToken(address(this)).initialize(
+            IERC20(0x0), // no underlying/wrapped token
+            18, // shouldn't matter if there's no wrapped token
+            name,
+            symbol
+        );
+        ISuperToken(address(this)).selfMint(msg.sender, initialSupply, new bytes(0));
+    }
+}

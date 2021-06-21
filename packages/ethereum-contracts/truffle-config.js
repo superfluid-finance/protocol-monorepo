405fb4b53874c3f5a1778ffaa2197753f9570a08
Miao ZhiCheng <miao@superfluid.finance>
2021-06-21 16:02:17 +0300

    higher gas price for goerli

--- packages/ethereum-contracts/truffle-config.js
+++ packages/ethereum-contracts/truffle-config.js
@@ -120,7 +120,7 @@ module.exports = {
                 ),
             network_id: 5,
             gas: 8e6,
-            gasPrice: +process.env.GOERLI_GAS_PRICE || 100e9, // 100 GWEI, goerli is busy!
+            gasPrice: +process.env.GOERLI_GAS_PRICE || 500e9, // goerli is busy!
             //confirmations: 6, // # of confs to wait between deployments. (default: 0)
             timeoutBlocks: 50, // # of blocks before a deployment times out  (minimum/default: 50)
             skipDryRun: false, // Skip dry run before migrations? (default: false for public nets )

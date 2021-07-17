a4cdd7e2cb39203ad0ead54fa6e3cc8bbf281484
Joshua Trujillo <Jtriley15@gmail.com>
2021-07-17 17:50:22 -0400

    general.js types declared

--- packages/js-sdk/src/utils/general.d.ts
+++ packages/js-sdk/src/utils/general.d.ts
@@ -1,10 +1,19 @@
-export function validateAddress(address: any): void;
-export function isAddress(address: any): boolean;
-export function getCleanAddress(address: any): any;
-export function completeTransaction({ sf, method, args, sender, onTransaction, }: {
-    sf: any;
-    method: any;
-    args: any;
-    sender: any;
-    onTransaction: any;
-}): Promise<any>;
+import type Framework from '../Framework'
+interface TX {
+    action: string
+    txHash: string
+    gas: BN
+    gasPrice: BN
+    cost: BN
+}
+
+export function validateAddress(address: string): void;
+export function isAddress(address: string): boolean;
+export function getCleanAddress(address: string): string;
+export function completeTransaction({ sf, method, args, sender, onTransaction }: {
+    sf: Framework;
+    method: () => TX;
+    args: Array<string>;
+    sender: string;
+    onTransaction: () => null;
+}): Promise<TX>;

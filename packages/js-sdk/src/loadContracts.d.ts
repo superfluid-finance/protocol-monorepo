338d47d584d5c986d33048ceb51a9a342a4abeb9
Drew Fisher <awfisher@wpi.edu>
2021-07-17 21:30:51 -0400

    Add types for loadContract

--- packages/js-sdk/src/loadContracts.d.ts
+++ packages/js-sdk/src/loadContracts.d.ts
@@ -1,10 +1,33 @@
-export = loadContracts;
+import { ethers, Contract, utils } from "ethers"; 
+import Web3 from "web3";
+import TruffleContract from "@truffle/contract";
+
+interface EthersWithSigner {
+    getSigner(): () => ethers.Signer
+}
+
+type AbiContainer = Pick<utils.Interface, "abi">;
+type ContractLoader = (name: string) => AbiContainer;
+
+declare function setTruffleContractDefaults(c: TruffleContract.Contract, networkId: number, from: string): void;
+
+declare function defaultContractLoader(name: string): {contractName: string, abi: AbiContainer}
+
+interface AdaptedContractOpts {
+    address: string;
+    abi: AbiContainer;
+    ethers: EthersWithSigner;
+}
+declare function getAdaptedContract({address, abi, ethers}: AdaptedContractOpts): Contract;
+
 declare function loadContracts({ isTruffle, ethers, web3, from, additionalContracts, contractLoader, networkId, }: {
-    isTruffle: any;
-    ethers: any;
-    web3: any;
-    from: any;
-    additionalContracts: any;
-    contractLoader: any;
-    networkId: any;
-}): Promise<{}>;
+    isTruffle: boolean;
+    ethers?: EthersWithSigner;
+    web3?: Web3;
+    from: string;
+    additionalContracts?: string[];
+    contractLoader: ContractLoader;
+    networkId: number;
+}): Promise<Contract[] | Web3.Eth.Contract[] | TruffleContract.Contract[]>;
+
+export = loadContracts;

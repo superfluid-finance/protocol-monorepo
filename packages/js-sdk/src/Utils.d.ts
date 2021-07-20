cb5940bcfcd889959bb14b6a6101dad2dbf2f626
manav2401 <manavdarji.india@gmail.com>
2021-07-21 02:03:35 +0530

    fix: added type for sf object in Utils

--- packages/js-sdk/src/Utils.d.ts
+++ packages/js-sdk/src/Utils.d.ts
@@ -1,7 +1,8 @@
+import Framework from "./Framework";
 import type BN from "bn.js";
 declare class Utils {
-    constructor(sf: any);
-    _sf: any;
+    constructor(sf: Framework);
+    _sf: Framework;
     normalizeTokenParam(param: string): string;
     normalizeAddressParam(param: string): string;
     normalizeFlowRateParam(param: BN | string): BN;

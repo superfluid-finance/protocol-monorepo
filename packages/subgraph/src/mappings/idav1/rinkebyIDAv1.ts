de108265897832f41a55c03a522a4e3b284d8844
0xdavinchee <0xdavinchee@gmail.com>
2021-09-20 10:15:00 +0300

    ida handlers updated
    - same pattern applied to ida patterns to pass in correct host address

---
+++ packages/subgraph/src/mappings/idav1/rinkebyIDAv1.ts
@@ -0,0 +1,44 @@
+import { Address } from "@graphprotocol/graph-ts";
+import {
+    IndexCreated as IndexCreatedEvent,
+    IndexUpdated as IndexUpdatedEvent,
+    SubscriptionApproved as SubscriptionApprovedEvent,
+    SubscriptionRevoked as SubscriptionRevokedEvent,
+    SubscriptionUnitsUpdated as SubscriptionUnitsUpdatedEvent,
+} from "../../../generated/InstantDistributionAgreementV1/IInstantDistributionAgreementV1";
+import { RINKEBY_HOST_ADDRESS } from "../../utils";
+import {
+    handleIndexCreated,
+    handleIndexUpdated,
+    handleSubscriptionApproved,
+    handleSubscriptionRevoked,
+    handleSubscriptionUnitsUpdated,
+} from "./idav1Base";
+
+let HOST_ADDRESS = Address.fromString(RINKEBY_HOST_ADDRESS);
+
+export function rinkebyHandleIndexCreated(event: IndexCreatedEvent): void {
+    handleIndexCreated(event, HOST_ADDRESS);
+}
+
+export function rinkebyHandleIndexUpdated(event: IndexUpdatedEvent): void {
+    handleIndexUpdated(event, HOST_ADDRESS);
+}
+
+export function rinkebyHandleSubscriptionApproved(
+    event: SubscriptionApprovedEvent
+): void {
+    handleSubscriptionApproved(event, HOST_ADDRESS);
+}
+
+export function rinkebyHandleSubscriptionRevoked(
+    event: SubscriptionRevokedEvent
+): void {
+    handleSubscriptionRevoked(event, HOST_ADDRESS);
+}
+
+export function rinkebyHandleSubscriptionUnitsUpdated(
+    event: SubscriptionUnitsUpdatedEvent
+): void {
+    handleSubscriptionUnitsUpdated(event, HOST_ADDRESS);
+}
---
+++ packages/subgraph/src/mappings/idav1/ropstenIDAv1.ts
@@ -0,0 +1,44 @@
+import { Address } from "@graphprotocol/graph-ts";
+import {
+    IndexCreated as IndexCreatedEvent,
+    IndexUpdated as IndexUpdatedEvent,
+    SubscriptionApproved as SubscriptionApprovedEvent,
+    SubscriptionRevoked as SubscriptionRevokedEvent,
+    SubscriptionUnitsUpdated as SubscriptionUnitsUpdatedEvent,
+} from "../../../generated/InstantDistributionAgreementV1/IInstantDistributionAgreementV1";
+import { ROPSTEN_HOST_ADDRESS } from "../../utils";
+import {
+    handleIndexCreated,
+    handleIndexUpdated,
+    handleSubscriptionApproved,
+    handleSubscriptionRevoked,
+    handleSubscriptionUnitsUpdated,
+} from "./idav1Base";
+
+let HOST_ADDRESS = Address.fromString(ROPSTEN_HOST_ADDRESS);
+
+export function ropstenHandleIndexCreated(event: IndexCreatedEvent): void {
+    handleIndexCreated(event, HOST_ADDRESS);
+}
+
+export function ropstenHandleIndexUpdated(event: IndexUpdatedEvent): void {
+    handleIndexUpdated(event, HOST_ADDRESS);
+}
+
+export function ropstenHandleSubscriptionApproved(
+    event: SubscriptionApprovedEvent
+): void {
+    handleSubscriptionApproved(event, HOST_ADDRESS);
+}
+
+export function ropstenHandleSubscriptionRevoked(
+    event: SubscriptionRevokedEvent
+): void {
+    handleSubscriptionRevoked(event, HOST_ADDRESS);
+}
+
+export function ropstenHandleSubscriptionUnitsUpdated(
+    event: SubscriptionUnitsUpdatedEvent
+): void {
+    handleSubscriptionUnitsUpdated(event, HOST_ADDRESS);
+}

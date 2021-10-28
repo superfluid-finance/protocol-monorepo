27df6fd9303514b7241cdf25eb8b85a00f13d966
0xdavinchee <0xdavinchee@gmail.com>
2021-10-28 10:23:14 +0300

    export all interfaces

--- packages/sdk-core/src/index.ts
+++ packages/sdk-core/src/index.ts
@@ -2,6 +2,8 @@ import Framework from "./Framework";
 import SuperToken from "./SuperToken";
 import Query from "./Query";
 
+export * from "./interfaces";
+
 export default {
     Framework,
     SuperToken,

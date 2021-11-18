22f70536039cc18956699d3580ecad873ee96421
0xdavinchee <0xdavinchee@gmail.com>
2021-11-18 14:59:43 +0200

    quotes

--- packages/subgraph/tasks/getSFMeta.sh
+++ packages/subgraph/tasks/getSFMeta.sh
@@ -3,8 +3,8 @@ TAG="$(git describe --abbrev=0 --tag)"
 BRANCH="$(git branch --show-current)"
 
 cat > src/meta.ignore.ts << EOF
-export let commitHash = ${COMMIT_HASH};
-export let configuration = ${CONFIGURATION};
-export let branch = ${BRANCH};
-export let tag = ${TAG};
+export let commitHash = "${COMMIT_HASH}";
+export let configuration = "${CONFIGURATION}";
+export let branch = "${BRANCH}";
+export let tag = "${TAG}";
 EOF

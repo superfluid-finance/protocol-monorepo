0087015cb8711f60bfa1da47ecf625360712dbc2
0xdavinchee <0xdavinchee@gmail.com>
2021-11-20 16:25:37 +0200

    ci cleanup attempt #1
    - pretest/posttest added to subgraph + sdk-core package.json
    - startGanacheAndDeployContracts task added
    - testenv-ctl added to sdk-core + subgraph follows the ethereum-contracts format
    - ci.canary.yml: removed a lot of the tasks for sdk-core as they are now in the startGanacheAndDeployContracts.sh and sdk-core testenv-ctl.sh
    - ci.feature.yaml: did the same for sdk-core here as above + did this for subgraph as well

---
+++ packages/subgraph/tasks/testenv-ctl.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+CMD=$1
+
+if [ "$CMD" == "start" ];then
+    cd ../sdk-core
+    ./tasks/startGanacheAndDeployContracts.sh ganache-only
+    # Generate Typechain files and move to subgraph directory
+    cd ../ethereum-contracts
+    yarn run generate-ethers-types
+    mv typechain ../subgraph
+    cd ../subgraph
+    # Get ABI for subgraph.yaml
+    node scripts/getAbi.js
+    cd ../js-sdk
+    # Get abi.js file for js-sdk to deploy locally
+    chmod +x ./tasks/build-abi-js.sh
+    ./tasks/build-abi-js.sh
+    cd ../subgraph
+    # Deploy contracts and token locally
+    yarn deploy-contracts-local
+    # Prepare, set network, build and deploy subgraph locally
+    yarn build-and-deploy-local
+elif [ "$CMD" == "stop" ];then
+    ./tasks/startGanacheAndDeployContracts.sh $CMD
+fi

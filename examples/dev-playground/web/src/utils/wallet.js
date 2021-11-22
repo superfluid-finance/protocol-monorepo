d9d5696812aeb7ff7200dfe9e2504b5bcb981abc
Miao ZhiCheng <miao@superfluid.finance>
2021-11-22 17:18:40 +0200

    remove dev-playground and react-ts-sdk

--- examples/dev-playground/web/src/utils/wallet.js
+++
@@ -1,9 +0,0 @@
-import { Web3Provider } from '@ethersproject/providers'
-
-import { unlockBrowser } from './browser'
-import { unlockWalletConnect, isWalletConnect } from './walletConnect'
-
-export const unlockWallet = async (options) => {
-  if (isWalletConnect()) return unlockWalletConnect(options)
-  return unlockBrowser(options)
-}

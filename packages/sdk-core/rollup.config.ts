0ff6669686992ea58c18da09e8045764f40f78d3
0xdavinchee <0xdavinchee@gmail.com>
2021-11-24 14:56:18 +0200

    cleanup wip
    - rollup config modified: no more umd + only one set of declaration files
    - setup moved out of src
    - flow rate helper functions fixed up
    - get flow amount per day/week/month/year given a per second flow rate

--- packages/sdk-core/rollup.config.ts
+++ packages/sdk-core/rollup.config.ts
@@ -1,22 +1,26 @@
 import typescript from "@rollup/plugin-typescript";
 import json from "@rollup/plugin-json";
 
-export default {
+const cjsConfig = {
     input: "src/index.ts",
     output: [
         {
             file: "dist/main/index.js",
             format: "cjs",
         },
+    ],
+    plugins: [typescript({ tsconfig: "./tsconfig.json" }), json()],
+};
+
+const esConfig = {
+    input: "src/index.ts",
+    output: [
         {
             file: "dist/module/index.js",
             format: "es",
         },
-        {
-            name: "cdn-js",
-            file: "dist/umd/index.js",
-            format: "umd",
-        },
     ],
-    plugins: [typescript({ tsconfig: "./tsconfig.json" }), json()],
+    plugins: [typescript(), json()],
 };
+
+export default [cjsConfig, esConfig];

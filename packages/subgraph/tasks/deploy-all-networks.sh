abc83937a68c65bd090e96016c007d2b4d4e87d7
0xdavinchee <0xdavinchee@gmail.com>
2021-11-30 17:05:24 +0200

    deploy to all networks fix

--- packages/subgraph/tasks/deploy-all-networks.sh
+++ packages/subgraph/tasks/deploy-all-networks.sh
@@ -10,12 +10,4 @@ CONTRACTS=( $($JQ -r .[] ./networks.json) )
 chmod +x ./tasks/deploy-to-network.sh
 for i in "${CONTRACTS[@]}";do
     ./tasks/deploy-to-network.sh $1 $i
-    # mustache config/$i.json subgraph.template.yaml > subgraph.yaml
-    # mustache config/$i.json src/addresses.template.ts > src/addresses.ts
-    # graph deploy
-    #     --product hosted-service \
-    #     superfluid-finance/protocol-$1-$i \
-    #     --node https://api.thegraph.com/deploy/ \
-    #     --ipfs https://api.thegraph.com/ipfs \
-    #     --access-token $THEGRAPH_ACCESS_TOKEN
 done

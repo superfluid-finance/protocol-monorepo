39509e43c8f255dd94b8cab83828117956d9bd60
Miao ZhiCheng <miao@superfluid.finance>
2021-12-08 13:16:13 +0200

    [skip ci] fix tasks/supports-etherscan.sh

--- packages/ethereum-contracts/tasks/supports-etherscan.sh
+++ packages/ethereum-contracts/tasks/supports-etherscan.sh
@@ -1,7 +1,12 @@
 TRUFFLE_NETWORK=$1
 
 case $TRUFFLE_NETWORK in
-    goerli | rinkeby | ropsten | kovan | mumbai | matic | opmainnet | opkovan | arbone | arbrinkeby | avalanche | avafuji | bsc )
+    eth-goerli | eth-rinkeby | eth-ropsten | eth-kovan | \
+    polygon-mumbai | polygon-matic | \
+    optimism-mainnet | optimism-kovan | \
+    arbitrum-one | arbitrum-rinkeby | \
+    avalanche-mainnet | avalance-fuji | \
+    bsc )
         echo "$TRUFFLE_NETWORK is supported by Etherscan or alike"
         exit 0;
         ;;

4faf813cc57c96cd7eddc07b0a0ab6bd907d0144
didi <git@d10r.net>
2022-01-20 11:49:03 +0100

    make skip-on-coverage work

--- packages/ethereum-contracts/.solcover.js
+++ packages/ethereum-contracts/.solcover.js
@@ -6,5 +6,9 @@ module.exports = {
         "mocks/",
         "apps",
         "libs/Strings.sol", // solidity test cases not included in coverage
-    ]
+    ],
+    mocha: {
+        grep: "@skip-on-coverage", // Find everything with this tag
+        invert: true               // Run the grep's inverse set.
+    }
 };

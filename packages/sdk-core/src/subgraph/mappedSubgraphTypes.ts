6fd8c2fe16a213e211b303324d700df3e20e475d
Kaspar Kallas <kaspar@superfluid.finance>
2022-01-27 11:07:39 +0200

    Remove not used types

--- packages/sdk-core/src/subgraph/mappedSubgraphTypes.ts
+++ packages/sdk-core/src/subgraph/mappedSubgraphTypes.ts
@@ -1,11 +1,7 @@
+// TODO(KK): Add comments
+
 export type BlockNumber = number;
 export type Timestamp = number;
 export type SubgraphId = string;
 export type Address = string;
 export type BigNumber = string;
-
-export type BlockNumberInput = string;
-export type TimestampInput = string;
-export type SubgraphIdInput = string;
-export type AddressInput = string;
-export type BigNumberInput = string;

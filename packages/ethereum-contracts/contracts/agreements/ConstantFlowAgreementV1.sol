b9d28014fcc99c6f15a8fcb337ebdea630b4c67d
davinchee <0xdavinchee@gmail.com>
2022-02-01 15:35:12 +0200

    typo

--- packages/ethereum-contracts/contracts/agreements/ConstantFlowAgreementV1.sol
+++ packages/ethereum-contracts/contracts/agreements/ConstantFlowAgreementV1.sol
@@ -122,7 +122,7 @@ contract ConstantFlowAgreementV1 is
             );
             return isPatricianPeriod
                 ? LiquidationPeriod.Patrician
-                : LiquidationPeriod.Pirate;
+                : LiquidationPeriod.Pleb;
         }
     }
 

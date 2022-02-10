5ace0c73cf141b6a67fcc7f3b57524c079f2c6fb
Kaspar Kallas <kaspar@superfluid.finance>
2022-02-10 12:59:00 +0200

    Remove unnecessary code

--- examples/sdk-redux-nextjs-typescript/pages/_app.tsx
+++ examples/sdk-redux-nextjs-typescript/pages/_app.tsx
@@ -1,17 +1,6 @@
-const {default: AbortController} = require("abort-controller");
-const {default: fetch, Headers, Request, Response} = require("node-fetch");
-
 import "../styles/globals.css";
 const {wrapper} = require("../redux/store");
 import type {AppProps} from "next/app";
-
-Object.assign(globalThis, {
-    fetch,
-    Headers,
-    Request,
-    Response,
-    AbortController,
-});
 
 export function MyApp({Component, pageProps}: AppProps) {
     return <Component {...pageProps} />;

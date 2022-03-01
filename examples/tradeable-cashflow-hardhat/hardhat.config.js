158619fb7bd4bd35a628e328b92e651224b9ed06
Sam Flamini <sam_flamini@Sams-MacBook-Air.local>
2022-03-01 17:19:54 -0500

    test suite, scripts, etc

--- examples/tradeable-cashflow-hardhat/hardhat.config.js
+++ examples/tradeable-cashflow-hardhat/hardhat.config.js
@@ -1,39 +1,41 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
- const HDWalletProvider = require("@truffle/hdwallet-provider");
- require("@nomiclabs/hardhat-truffle5");
+require("@nomiclabs/hardhat-truffle5");
+ require("@nomiclabs/hardhat-ethers");
 require('hardhat-deploy');
 
  require("dotenv").config();
  const GAS_LIMIT = 8000000;
 
-
+//  const defaultNetwork = 'kovan';
  
  module.exports = {
-     networks: {
-         ganache: {
-             host: "127.0.0.1",
-             network_id: "*",
-             port: process.env.GANACHE_PORT || 8545,
-         },
+  // defaultNetwork,
+  networks: {
+    // ganache: {
+    //     url: "127.0.0.1",
+    //     port: process.env.GANACHE_PORT || 8545,
+    // },
 
     kovan: {
-      url: `${process.env.KOVAN_ALCHEMY_URL}`,
+      url: `${process.env.KOVAN_RPC_URL}`,
+      accounts: [`${process.env.DEPLOYER_PRIVATE_KEY}`],
       gas: GAS_LIMIT,
-      gasPrice:  1500000000,
-      timeoutBlocks: 50,
-      skipDryRun: false,
-      accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`]
+      gasPrice: 11e9, // 10 GWEI
+      confirmations: 6, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 50, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: false // Skip dry run before migrations? (default: false for public nets )
       },
     },
  
      solidity: {
-      version: "^0.7.6",
+      version: "0.7.6",
       settings: {
         optimizer: {
           enabled: true
         }
       }
-    }
+    },
+      namedAccounts: {
+    deployer: 0
+  }
+
   }

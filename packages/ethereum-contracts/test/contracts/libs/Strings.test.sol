5a1654b17c3b30df2621794dd7cb5eae52f624cf
xdavinchee <0xdavinchee@gmail.com>
2022-03-05 13:26:28 +0200

    Update Strings.test.sol

--- packages/ethereum-contracts/test/contracts/libs/Strings.test.sol
+++ packages/ethereum-contracts/test/contracts/libs/Strings.test.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPLv3
 // solhint-disable
 // ADAPTED FROM: https://github.com/Arachnid/solidity-stringutils/
-pragma solidity 0.7.6;
+pragma solidity 0.8.12;
 
 import '../../../contracts/libs/Strings.sol';
 import "truffle/Assert.sol";

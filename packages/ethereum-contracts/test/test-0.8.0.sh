e785e9797da8d9ce6fc081f336e4da6a3ddf7416
xdavinchee <0xdavinchee@gmail.com>
2022-03-07 20:44:24 +0200

    testing 0.8.0 compatibility, not 0.7.4

---
+++ packages/ethereum-contracts/test/test-0.8.0.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# make sure that if any step fails, the script fails
+set -xe
+
+# go to contracts folder
+cd ../contracts
+# yarn add @openzeppelin/contracts@4.5.0
+
+mkdir bin
+wget https://github.com/ethereum/solc-bin/raw/gh-pages/macosx-amd64/solc-macosx-amd64-v0.8.0%2Bcommit.c7dfd78e -O bin/solc-0.8.0
+
+# sha256sum -c solc.sha256
+
+chmod +x bin/solc-0.8.0
+
+ln -s ../../../node_modules/@openzeppelin .
+
+find interfaces/ -name '*.sol' | while read i;do
+  echo $i
+  bin/solc-0.8.0 --allow-paths . $i
+done
+
+echo SUCCESS

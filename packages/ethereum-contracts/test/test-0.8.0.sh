3bc8929ac4f65185d48d645dd7e4e495ac908464
davinchee <0xdavinchee@gmail.com>
2022-03-08 14:18:43 +0200

    test if sha256sum exists

--- packages/ethereum-contracts/test/test-0.8.0.sh
+++ packages/ethereum-contracts/test/test-0.8.0.sh
@@ -5,12 +5,11 @@ set -xe
 
 # go to contracts folder
 cd ./contracts
-yarn add @openzeppelin/contracts@4.5.0
 
 mkdir bin
 wget https://github.com/ethereum/solc-bin/raw/gh-pages/linux-amd64/solc-linux-amd64-v0.8.0%2Bcommit.c7dfd78e -O bin/solc-0.8.0
 
-# sha256sum -c solc.sha256
+sha256sum -c solc.sha256
 
 chmod +x bin/solc-0.8.0
 

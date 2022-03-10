f088b77ad9a8c8e58fc45214ff81d1c98447a607
Miao ZhiCheng <miao@superfluid.finance>
2022-03-10 15:21:57 +0200

    informative revertFromReturnedData

--- packages/ethereum-contracts/contracts/libs/CallUtils.sol
+++ packages/ethereum-contracts/contracts/libs/CallUtils.sol
@@ -7,6 +7,50 @@ pragma solidity 0.8.12;
  */
 library CallUtils {
 
+    /// @dev Bubble up the revert from the returnedData (supports Panic, Error & Custom Errors)
+    /// @notice This is needed in order to get the human-readable revert message from a call
+    /// @param res Response of the call
+    function revertFromReturnedData(bytes memory returnedData) internal pure {
+        if (returnedData.length < 4) {
+            // case 1: catch all
+            revert("CallUtils: target revert()");
+        } else {
+            bytes4 errorSelector;
+            assembly {
+                errorSelector := mload(add(returnedData, 0x20))
+            }
+            //revert RePanic(uint256(uint32(errorSelector)));
+            if (errorSelector == bytes4(0x4e487b71) /* `seth sig "Panic(uint256)"` */) {
+                // case 2: Panics(uint256) (Defined since 0.8.0)
+                // solhint-disable-next-line max-line-length
+                // ref: https://docs.soliditylang.org/en/v0.8.0/control-structures.html#panic-via-assert-and-error-via-require)
+                string memory reason = "CallUtils: target panicked: 0x__";
+                uint errorCode;
+                assembly {
+                    errorCode := mload(add(returnedData, 0x24))
+                    let reasonWord := mload(add(reason, 0x20))
+                    // [0..9] is converted to ['0'..'9']
+                    // [0xa..0xf] is not correctly convereted to ['a'..'f']
+                    // but since panic code doesn't have those cases, we will ignore them for now!
+                    let e1 := add(and(errorCode, 0xf), 0x30)
+                    let e2 := shl(8, add(shr(4, and(errorCode, 0xf0)), 0x30))
+                    reasonWord := or(
+                        and(reasonWord, 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0000),
+                        or(e2, e1))
+                    mstore(add(reason, 0x20), reasonWord)
+                }
+                revert(reason);
+            } else {
+                // case 3: Error(string) (Defined since 0.7.0)
+                // case 4: Custom errors (Defined since 0.8.0)
+                uint len = returnedData.length;
+                assembly {
+                    revert(add(returnedData, 32), len)
+                }
+            }
+        }
+    }
+
     /// @dev Get the revert message from a call
     /// @notice This is needed in order to get the human-readable revert message from a call
     /// @param res Response of the call

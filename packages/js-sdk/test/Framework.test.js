0f2052c3d202c1ed45f9a565cf50c9ce4f4a383b
didi <git@d10r.net>
2022-03-16 11:08:05 +0100

    fixed test

--- packages/js-sdk/test/Framework.test.js
+++ packages/js-sdk/test/Framework.test.js
@@ -31,10 +31,15 @@ describe("Framework class", function () {
             isTruffle: false,
             web3,
         });
-        await deploySuperToken(t.createErrorHandler(), [":", "ETH"], {
-            isTruffle: false,
-            web3,
-        });
+        // wrapper for the native token
+        await deploySuperToken(
+            t.createErrorHandler(),
+            [":", "0x0000000000000000000000000000000000000000"],
+            {
+                isTruffle: false,
+                web3,
+            }
+        );
         await deploySuperToken(t.createErrorHandler(), [":", "fDAI"], {
             isTruffle: false,
             web3,

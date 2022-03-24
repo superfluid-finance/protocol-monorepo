997079b2291db1961bb338d69d1fde3babf6f5c3
davinchee <0xdavinchee@gmail.com>
2022-03-24 11:22:34 +0200

    update to 0.8.13

--- packages/ethereum-contracts/contracts/mocks/CFALibraryMock.sol
+++ packages/ethereum-contracts/contracts/mocks/CFALibraryMock.sol
@@ -1,5 +1,5 @@
-//SPDX-License-Identifier: MIT
-pragma solidity 0.8.12;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
 
 import {ISuperfluid, ISuperfluidToken, ISuperToken} from "../interfaces/superfluid/ISuperfluid.sol";
 

cefc077d0989934b5ce64504668e2a562eada843
xdavinchee <0xdavinchee@gmail.com>
2022-05-17 21:11:13 +0300

    fix build

--- packages/sdk-core/test/5_subgraph.test.ts
+++ packages/sdk-core/test/5_subgraph.test.ts
@@ -1,25 +1,16 @@
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { SuperToken as SuperTokenType } from "../src/typechain";
-import { Framework, WrapperSuperToken } from "../src";
-import { ROPSTEN_SUBGRAPH_ENDPOINT } from "./0_framework.test";
+import { Framework } from "../src";
 import { setup } from "../scripts/setup";
 
 describe("Subgraph Tests", () => {
     let framework: Framework;
-    let deployer: SignerWithAddress;
-    let superToken: SuperTokenType;
-    let daix: WrapperSuperToken;
 
     before(async () => {
         const subgraphEndpoint =
             "https://api.thegraph.com/subgraphs/name/superfluid-finance/protocol-v1-matic";
-        const { frameworkClass, Deployer, SuperToken } = await setup({
+        const { frameworkClass } = await setup({
             subgraphEndpoint,
         });
         framework = frameworkClass;
-        deployer = Deployer;
-        superToken = SuperToken;
-        daix = await framework.loadWrapperSuperToken(superToken.address);
     });
 
     it("Should be able to make the getAllEvents query", async () => {

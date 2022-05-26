98272021f1ab58a573d91a9da7b860c7b1028796
Sam Flamini <sam_flamini@Sams-MacBook-Air.local>
2022-05-26 17:23:58 -0500

    comment out networks in hardhat.config so tests pass

--- examples/rewards-distribution-token/hardhat.config.ts
+++ examples/rewards-distribution-token/hardhat.config.ts
@@ -27,15 +27,15 @@ const { API_URL, PRIVATE_KEY } = process.env;
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  defaultNetwork,
+  // defaultNetwork,
   solidity: "0.8.13",
-  networks: {
-    goerli: {
-      url: process.env.RPC_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-  },
+  // networks: {
+  //   goerli: {
+  //     url: process.env.RPC_URL || "",
+  //     accounts:
+  //       process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+  //   },
+  // },
   
   // etherscan: {
   //   apiKey: process.env.ETHERSCAN_API_KEY,

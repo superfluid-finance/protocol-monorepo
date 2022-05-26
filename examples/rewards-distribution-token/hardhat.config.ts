e7914b38d265716276d79349eb6f81cbb646526f
davinchee <0xdavinchee@gmail.com>
2022-05-26 12:04:26 +0300

    cleanup of tests file + config and removal of abi

--- examples/rewards-distribution-token/hardhat.config.ts
+++ examples/rewards-distribution-token/hardhat.config.ts
@@ -1,6 +1,6 @@
 import * as dotenv from "dotenv";
 
-import { HardhatUserConfig, task } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-truffle5";
 import "@nomiclabs/hardhat-ethers";
@@ -10,34 +10,11 @@ import "solidity-coverage";
 
 dotenv.config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-const { API_URL, PRIVATE_KEY } = process.env;
-module.exports = {
-  solidity: "0.8.13",
-  // defaultNetwork: "localhost",
-  // networks: {
-  //    hardhat: {},
-  //    ropsten: {
-  //       url: API_URL,
-  //       accounts: [`0x${PRIVATE_KEY}`]
-  //    }
-  // },
-}
-
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  solidity: "0.8.13",
   // networks: {
   //   ropsten: {
   //     url: process.env.ROPSTEN_URL || "",
---
+++ examples/rewards-distribution-token/types/custom.d.ts
@@ -0,0 +1,5 @@
+declare module "@superfluid-finance/ethereum-contracts/scripts/deploy-framework";
+declare module "@superfluid-finance/ethereum-contracts/scripts/deploy-test-token";
+declare module "@superfluid-finance/ethereum-contracts/scripts/deploy-super-token";
+declare module "@decentral.ee/web3-helpers";
+declare interface web3 {}

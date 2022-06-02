22262f136150fde3b0f22ac18e83894308affa22
Sam Flamini <sam_flamini@Sams-MacBook-Air.local>
2022-06-02 08:56:48 -0500

    add callback checks and remove state var redundancy

--- examples/employment-based-loan/contracts/test/MockV3Aggregator.sol
+++ examples/employment-based-loan/contracts/test/MockV3Aggregator.sol
@@ -1,34 +1,9 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
 import { ISuperToken } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol"; 
 
 import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-
-// contract UniV3Observation {
-
-//     //To Do
-//     //need to use ticks to get the correct price...
-//     // int160 currentPrice;
-//     // address tokenAddress;
-//     mapping (ISuperToken => uint) tokenPrices;
-
-//     constructor(uint _initialPrice, ISuperToken _token) {
-//         // currentPrice = _initialPrice;
-//         // tokenAddress = _tokenAddress;
-//         tokenPrices[_token] = _initialPrice;
-//     } 
-
-//     function setPrice(uint currentPrice, ISuperToken _token) public {
-//         // currentPrice = _initialPrice;
-//         // tokenAddress = _tokenAddress;
-//         tokenPrices[_token] = currentPrice;
-//     }
-
-//     function getCurrentPrice(ISuperToken token) external view returns (uint) {
-//         return tokenPrices[token];
-//     }
-// }
-
 
 contract MockV3Aggregator {
     int256 public _latestRoundData;

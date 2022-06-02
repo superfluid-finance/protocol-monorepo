9699500032324cfd7be9c6263e11fa97dce56528
Sam Flamini <sam_flamini@Sams-MacBook-Air.local>
2022-06-02 09:50:40 -0500

    clean up mockv3aggregator file

--- examples/employment-based-loan/contracts/test/MockV3Aggregator.sol
+++ examples/employment-based-loan/contracts/test/MockV3Aggregator.sol
@@ -1,16 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
-import { ISuperToken } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol"; 
-
-import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-
 contract MockV3Aggregator {
     int256 public _latestRoundData;
 
     event AnswerUpdated(int256 indexed current, uint256 indexed roundId, uint256 timestamp);
 
-    constructor (int256 _initialAnswer) public  {
+    constructor (int256 _initialAnswer)  {
         _latestRoundData = _initialAnswer;
         emit AnswerUpdated(_initialAnswer, 0, block.timestamp);
     }

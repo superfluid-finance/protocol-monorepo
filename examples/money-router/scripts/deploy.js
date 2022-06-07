af2296ef640cd4b9ca8d87b85d10794eab2e05c0
Sam Flamini <sam_flamini@Sams-MacBook-Air.local>
2022-06-07 16:04:05 -0500

    clean comments

--- examples/money-router/scripts/deploy.js
+++ examples/money-router/scripts/deploy.js
@@ -1,9 +1,3 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-
 const hre = require("hardhat");
 const { Framework } = require("@superfluid-finance/sdk-core");
 require("dotenv").config();

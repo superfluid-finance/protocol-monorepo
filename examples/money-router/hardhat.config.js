95fbfb7411d2e721c719e174d9a1dc39df2fce22
Sam Flamini <sam_flamini@Sams-MacBook-Air.local>
2022-06-07 16:03:34 -0500

    add scripts

--- examples/money-router/hardhat.config.js
+++ examples/money-router/hardhat.config.js
@@ -19,14 +19,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
+// const defaultNetwork = "goerli";
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.13",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
+    goerli: {
+      url: process.env.GOERLI_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
--- examples/money-router/scripts/deploy.js
+++ examples/money-router/scripts/deploy.js
@@ -3,8 +3,15 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
 
+const hre = require("hardhat");
+const { Framework } = require("@superfluid-finance/sdk-core");
+require("dotenv").config();
+
+//to run this script:
+//1) Make sure you've created your own .env file
+//2) Make sure that you have your network specified in hardhat.config.js
+//3) run: npx hardhat run scripts/deploy.js --network goerli
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
   // line interface.
@@ -13,13 +20,24 @@ async function main() {
   // manually to make sure everything is compiled
   // await hre.run('compile');
 
+  const provider = new hre.ethers.providers.JsonRpcProvider(process.env.GOERLI_URL);
+
+  const sf = await Framework.create({
+    chainId: (await provider.getNetwork()).chainId,
+    provider,
+    customSubgraphQueriesEndpoint: "",
+    dataMode: "WEB3_ONLY"
+  });
+
+  const signers = await hre.ethers.getSigners();
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const MoneyRouter = await hre.ethers.getContractFactory("MoneyRouter");
+  //deploy the money router account using the proper host address and the address of the first signer
+  const moneyRouter = await MoneyRouter.deploy(sf.settings.config.hostAddress, signers[0].address);
 
-  await greeter.deployed();
+  await moneyRouter.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("MoneyRouter deployed to:", moneyRouter.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere

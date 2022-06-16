fae69e05fdde9d43fa95752916cb9a6737a797e8
Miao ZhiCheng <miao@superfluid.finance>
2022-06-16 11:58:33 +0300

    typo

--- packages/ethereum-contracts/tasks/etherscan-verify-framework.sh
+++ packages/ethereum-contracts/tasks/etherscan-verify-framework.sh
@@ -38,7 +38,7 @@ echo NETWORK_ID=$NETWORK_ID
 FAILED_VERIFICATIONS=()
 function try_verify() {
     npx truffle --network $TRUFFLE_NETWORK run verify "$@"
-    # NOTE: append using length so that space in the element is note a problem
+    # NOTE: append using length so that having spaces in the element is not a problem
     [ $? != 0 ] && FAILED_VERIFICATIONS[${#FAILED_VERIFICATIONS[@]}]="$@"
 }
 

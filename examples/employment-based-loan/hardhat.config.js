68512a9e7c72a56e18ae9c886a98ee9449deab0b
Sam Flamini <sam_flamini@Sams-MacBook-Air.local>
2022-06-18 17:42:27 -0500

    fix compiler issues in other examples

--- examples/employment-based-loan/hardhat.config.js
+++ examples/employment-based-loan/hardhat.config.js
@@ -21,7 +21,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.0",
+  solidity: "0.8.14",
   settings: {
     optimizer: {
       enabled: true,
--- examples/tradeable-cashflow/tradeable-cashflow-truffle/contracts/TradeableCashflow.sol
+++ examples/tradeable-cashflow/tradeable-cashflow-truffle/contracts/TradeableCashflow.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
+pragma solidity >=0.8.13;
 
 import {RedirectAll, ISuperToken, ISuperfluid} from "./RedirectAll.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";

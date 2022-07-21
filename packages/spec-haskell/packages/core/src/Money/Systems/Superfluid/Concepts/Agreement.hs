ec8898a63915d44c2bd22c3fb8b63b20131694e1
Miao ZhiCheng <miao@superfluid.finance>
2022-07-22 02:08:38 +0300

    minor cleanup

--- packages/spec-haskell/packages/core/src/Money/Systems/Superfluid/Concepts/Agreement.hs
+++ packages/spec-haskell/packages/core/src/Money/Systems/Superfluid/Concepts/Agreement.hs
@@ -24,29 +24,6 @@ class ( SuperfluidTypes sft
         -> SFT_RTB sft   -- rtb
 
 -- | Agreement contract data type class.
---
--- Note the usage of functional dependency: the type of the monetary unit data is uniquely determined by the type of the
--- contract data.
--- class ( SuperfluidTypes sft
---       , AgreementMonetaryUnitData amud sft
---       , Default acd
---       , Applicative (AgreementContractPartiesF acd), Traversable (AgreementContractPartiesF acd)
---       ) => AgreementContractData acd amud sft | acd -> sft, acd -> amud where
---     -- | Agreement contract parties traversable applicative functor type.
---     data AgreementContractPartiesF acd elem :: Type
-
---     -- | Agreement operation algebraic data type.
---     data AgreementOperation acd :: Type
-
---     -- | ω function - applying agreement operation (hear: ω) to a agreement contract data to get agreement account data
---     --   delta of the agreement contract parties.
---     applyAgreementOperation
---         :: acd                                        -- acd
---         -> AgreementOperation acd                     -- ao
---         -> SFT_TS (sft)                               -- t
---         -> (acd, AgreementContractPartiesF acd amud)  -- (acd', acpΔamud)
-
--- | Agreement contract data type class.
 class (SuperfluidTypes sft , Default acd) => AgreementContractData acd sft | acd -> sft
 
 -- | Agreement operation type class.

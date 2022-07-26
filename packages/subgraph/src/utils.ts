0bb5e35236af2ceb2f4abbf98a255fcad423a7a0
davinchee <0xdavinchee@gmail.com>
2022-07-26 13:36:49 +0300

    SDK-Core - isNativeAssetSuperToken query support (#970)
    * fix isNativeAssetSuperToken indexing
    - use TypedMap instead of a bunch of ifs
    - hardcode addresses instead of symbol
    - formatting cleanup
    - optimize testenv-ctl.sh more for subgraph testing
    Co-authored-by: Miao ZhiCheng <miao@superfluid.finance>

--- packages/subgraph/src/utils.ts
+++ packages/subgraph/src/utils.ts
@@ -1,8 +1,12 @@
-import {Address, BigInt, Bytes, ethereum, log,} from "@graphprotocol/graph-ts";
-import {ISuperToken as SuperToken} from "../generated/templates/SuperToken/ISuperToken";
-import {Resolver} from "../generated/ResolverV1/Resolver";
-import {IndexSubscription, StreamRevision, Token, TokenStatistic,} from "../generated/schema";
-import { getNativeAssetSuperTokenSymbol } from "./addresses.template";
+import { Address, BigInt, Bytes, ethereum, log } from "@graphprotocol/graph-ts";
+import { ISuperToken as SuperToken } from "../generated/templates/SuperToken/ISuperToken";
+import { Resolver } from "../generated/ResolverV1/Resolver";
+import {
+    IndexSubscription,
+    StreamRevision,
+    Token,
+    TokenStatistic,
+} from "../generated/schema";
 
 /**************************************************************************
  * Constants
@@ -41,15 +45,27 @@ export function createEventID(
  * HOL entities util functions
  *************************************************************************/
 
-export function getTokenInfoAndReturn(
+export function handleTokenRPCCalls(
     token: Token,
-    tokenAddress: Address
+    resolverAddress: Address
 ): Token {
-    let tokenContract = SuperToken.bind(tokenAddress);
-    let underlyingAddressResult = tokenContract.try_getUnderlyingToken();
-    let nameResult = tokenContract.try_name();
-    let symbolResult = tokenContract.try_symbol();
-    let decimalsResult = tokenContract.try_decimals();
+    token = getIsListedToken(token, resolverAddress);
+
+    // we must handle the case when the native token hasn't been initialized
+    // there is no name/symbol, but this may occur later
+    if (token.name.length == 0 || token.symbol.length == 0) {
+        token = getTokenInfoAndReturn(token);
+    }
+    return token;
+}
+
+export function getTokenInfoAndReturn(token: Token): Token {
+    const tokenAddress = Address.fromString(token.id);
+    const tokenContract = SuperToken.bind(tokenAddress);
+    const underlyingAddressResult = tokenContract.try_getUnderlyingToken();
+    const nameResult = tokenContract.try_name();
+    const symbolResult = tokenContract.try_symbol();
+    const decimalsResult = tokenContract.try_decimals();
     token.underlyingAddress = underlyingAddressResult.reverted
         ? ZERO_ADDRESS
         : underlyingAddressResult.value;
@@ -57,17 +73,11 @@ export function getTokenInfoAndReturn(
     token.symbol = symbolResult.reverted ? "" : symbolResult.value;
     token.decimals = decimalsResult.reverted ? 0 : decimalsResult.value;
 
-    const nativeAssetSuperTokenSymbol = getNativeAssetSuperTokenSymbol();
-    if (token.symbol == nativeAssetSuperTokenSymbol) {
-        token.isNativeAssetSuperToken = true;
-    }
-
     return token;
 }
 
 export function getIsListedToken(
     token: Token,
-    tokenAddress: Address,
     resolverAddress: Address
 ): Token {
     let resolverContract = Resolver.bind(resolverAddress);
@@ -79,8 +89,9 @@ export function getIsListedToken(
         "supertokens." + version + "." + token.symbol
     );
     let superTokenAddress = result.reverted ? ZERO_ADDRESS : result.value;
-    token.isListed = tokenAddress.toHex() == superTokenAddress.toHex();
-    return token as Token;
+    token.isListed = token.id == superTokenAddress.toHex();
+
+    return token;
 }
 
 export function updateTotalSupplyForNativeSuperToken(
@@ -260,7 +271,7 @@ export function calculateMaybeCriticalAtTimestamp(
     updatedAtTimestamp: BigInt,
     balanceUntilUpdatedAt: BigInt,
     totalNetFlowRate: BigInt,
-    previousMaybeCriticalAtTimestamp: BigInt | null,
+    previousMaybeCriticalAtTimestamp: BigInt | null
 ): BigInt | null {
     // When the flow rate is not negative then there's no way to have a critical balance timestamp anymore.
     if (totalNetFlowRate.ge(BIG_INT_ZERO)) return null;
@@ -268,10 +279,14 @@ export function calculateMaybeCriticalAtTimestamp(
     // When there's no balance then that either means:
     // 1. account is already critical, and we keep the existing timestamp when the liquidations supposedly started
     // 2. it's a new account without a critical balance timestamp to begin with
-    if (balanceUntilUpdatedAt.le(BIG_INT_ZERO)) return previousMaybeCriticalAtTimestamp;
+    if (balanceUntilUpdatedAt.le(BIG_INT_ZERO))
+        return previousMaybeCriticalAtTimestamp;
 
-    const secondsUntilCritical = balanceUntilUpdatedAt.div(totalNetFlowRate.abs());
-    const calculatedCriticalTimestamp = updatedAtTimestamp.plus(secondsUntilCritical);
+    const secondsUntilCritical = balanceUntilUpdatedAt.div(
+        totalNetFlowRate.abs()
+    );
+    const calculatedCriticalTimestamp =
+        updatedAtTimestamp.plus(secondsUntilCritical);
     if (calculatedCriticalTimestamp.gt(MAX_SAFE_SECONDS)) {
         return MAX_SAFE_SECONDS;
     }
@@ -283,10 +298,7 @@ export function calculateMaybeCriticalAtTimestamp(
  * @param blockNumber
  * @param logIndex
  */
-export function getOrder(
-    blockNumber: BigInt,
-    logIndex: BigInt,
-): BigInt {
+export function getOrder(blockNumber: BigInt, logIndex: BigInt): BigInt {
     return blockNumber.times(ORDER_MULTIPLIER).plus(logIndex);
 }
 
@@ -297,15 +309,15 @@ export function getOrder(
 export function createLogID(
     logPrefix: string,
     accountTokenSnapshotId: string,
-    event: ethereum.Event,
+    event: ethereum.Event
 ): string {
     return (
-        logPrefix
-        + "-" +
-        accountTokenSnapshotId
-        + "-" +
-        event.transaction.hash.toHexString()
-        + "-" +
+        logPrefix +
+        "-" +
+        accountTokenSnapshotId +
+        "-" +
+        event.transaction.hash.toHexString() +
+        "-" +
         event.logIndex.toString()
     );
 }

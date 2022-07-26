f39efb8d08c38311efa9663079f9df1d5eb85ac3
0xdavinchee <0xdavinchee@gmail.com>
2022-07-26 15:43:00 +0300

    [SDK-Core] Call App Action Support Added/Fixed CallAppAction BatchCall (#995)
    * callAppAction support implemented
    
    * Call App Action Operation creator added in Host class
    * Call App Action support in batchCall added
    * SuperAppTester basic contract added
    * tests added for:
        * call app action executed from operation
        * call app action executed from batch call
    * getCallAgreementFunctionArgs deprecated and removed (BREAKING)
    * getCallDataFunctionArgs is replacing it
    * CHANGELOG updated and formatting cleanup
    * yarn.lock updated
    
    * cleanup refactor
    
    * BatchCall cleanup
    * compile contracts in setup to generate typechain-types files
    
    * Revert "cleanup refactor"
    
    reverts part of the previous commit
    
    * fix sferror + update version
    
    * changelog updated too
    
    * unreleased and 0.5.1
    
    * rename host functions

--- packages/sdk-core/hardhat.config.ts
+++ packages/sdk-core/hardhat.config.ts
@@ -13,7 +13,7 @@ dotenvConfig();
  */
 const config: HardhatUserConfig = {
     solidity: {
-        version: "0.7.6",
+        version: "0.8.15",
         settings: {
             optimizer: {
                 enabled: true,

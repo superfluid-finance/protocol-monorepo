ae17e4556d28e71c5699d6a5f9e188e655f662bb
davinchee <0xdavinchee@gmail.com>
2022-08-10 18:41:20 +0300

    use metadata in subgraph

--- packages/subgraph/scripts/maps.ts
+++ packages/subgraph/scripts/maps.ts
@@ -1,103 +1,20 @@
-import goerliAddresses from "../config/goerli.json";
-import optimismGoerliAddresses from "../config/optimism-goerli.json";
-import optimismMainnetAddresses from "../config/optimism-mainnet.json";
-import xdaiAddresses from "../config/xdai.json";
-import maticAddresses from "../config/matic.json";
-import avalancheFujiAddresses from "../config/avalanche-fuji.json";
-import avalancheCAddresses from "../config/avalanche-c.json";
-import mumbaiAddresses from "../config/mumbai.json";
-import arbitrumGoerliAddresses from "../config/arbitrum-goerli.json";
-import arbitrumOneAddresses from "../config/arbitrum-one.json";
+import sfMetaData from "@superfluid-finance/metadata";
 
-export const chainIdToData = new Map([
-    [
-        5,
+export const chainIdToData = new Map(
+    sfMetaData.networks.map((x) => [
+        x.chainId,
         {
-            subgraphAPIEndpoint:
-                "https://api.thegraph.com/subgraphs/name/superfluid-finance/protocol-dev-goerli",
-            name: "goerli",
-            addresses: goerliAddresses,
+            subgraphAPIEndpoint: x.subgraphV1.hostedEndpoint,
+            name: x.shortName,
+            addresses: {
+                network: x.shortName,
+                hostStartBlock: x.startBlockV1,
+                hostAddress: x.contractsV1.host,
+                cfaAddress: x.contractsV1.cfaV1,
+                idaAddress: x.contractsV1.idaV1,
+                superTokenFactoryAddress: x.contractsV1.superTokenFactory,
+                resolverV1Address: x.contractsV1.resolver,
+            },
         },
-    ],
-    [
-        10,
-        {
-            subgraphAPIEndpoint:
-                "https://api.thegraph.com/subgraphs/name/superfluid-finance/protocol-dev-optimism-mainnet",
-            name: "optimism-mainnet",
-            addresses: optimismMainnetAddresses,
-        },
-    ],
-    [
-        69,
-        {
-            subgraphAPIEndpoint:
-                "https://api.thegraph.com/subgraphs/name/superfluid-finance/protocol-dev-optimism-kovan",
-            name: "optimism-kovan",
-            addresses: optimismGoerliAddresses,
-        },
-    ],
-    [
-        100,
-        {
-            subgraphAPIEndpoint:
-                "https://api.thegraph.com/subgraphs/name/superfluid-finance/protocol-dev-xdai",
-            name: "xdai",
-            addresses: xdaiAddresses,
-        },
-    ],
-    [
-        137,
-        {
-            subgraphAPIEndpoint:
-                "https://api.thegraph.com/subgraphs/name/superfluid-finance/protocol-dev-matic",
-            name: "matic",
-            addresses: maticAddresses,
-        },
-    ],
-    [
-        42161,
-        {
-            subgraphAPIEndpoint:
-                "https://api.thegraph.com/subgraphs/name/superfluid-finance/protocol-dev-arbitrum-one",
-            name: "arbitrum-one",
-            addresses: arbitrumOneAddresses,
-        },
-    ],
-    [
-        43113,
-        {
-            subgraphAPIEndpoint:
-                "https://api.thegraph.com/subgraphs/name/superfluid-finance/protocol-dev-avalanche-fuji",
-            name: "avalanche-fuji",
-            addresses: avalancheFujiAddresses,
-        },
-    ],
-    [
-        43114,
-        {
-            subgraphAPIEndpoint:
-                "https://api.thegraph.com/subgraphs/name/superfluid-finance/protocol-dev-avalanche-c",
-            name: "avalanche-c",
-            addresses: avalancheCAddresses,
-        },
-    ],
-    [
-        80001,
-        {
-            subgraphAPIEndpoint:
-                "https://api.thegraph.com/subgraphs/name/superfluid-finance/protocol-dev-mumbai",
-            name: "mumbai",
-            addresses: mumbaiAddresses,
-        },
-    ],
-    [
-        421611,
-        {
-            subgraphAPIEndpoint:
-                "https://api.thegraph.com/subgraphs/name/superfluid-finance/protocol-dev-arbitrum-rinkeby",
-            name: "arbitrum-rinkeby",
-            addresses: arbitrumGoerliAddresses,
-        },
-    ],
-]);
+    ])
+);

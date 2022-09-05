30c3ac84127675912e13ce9b1db5ba4eecb92440
davinchee <0xdavinchee@gmail.com>
2022-09-05 15:16:17 +0300

    TS WIP - Add JS-SDK tests
    * add tests that exist in js-sdk to sdk-core as part of deprecation

--- packages/sdk-core/test/3_batch_call.test.ts
+++ packages/sdk-core/test/3_batch_call.test.ts
@@ -1,6 +1,6 @@
 import { expect } from "chai";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { Framework } from "../src/index";
+import { Framework, SuperToken, WrapperSuperToken } from "../src/index";
 import { getPerSecondFlowRateByMonth } from "../src";
 import { SuperToken as SuperTokenType } from "../src/typechain";
 import { setup } from "../scripts/setup";
@@ -11,6 +11,7 @@ import { createCallAppActionOperation } from "./2_operation.test";
 
 describe("Batch Call Tests", () => {
     let evmSnapshotId: string;
+    let daix: WrapperSuperToken;
     let framework: Framework;
     let deployer: SignerWithAddress;
     let alpha: SignerWithAddress;
@@ -29,6 +30,7 @@ describe("Batch Call Tests", () => {
         bravo = Bravo;
         charlie = Charlie;
         superToken = SuperToken;
+        daix = await framework.loadWrapperSuperToken(superToken.address);
         evmSnapshotId = await hre.network.provider.send("evm_snapshot");
     });
 
@@ -37,8 +39,52 @@ describe("Batch Call Tests", () => {
         evmSnapshotId = await hre.network.provider.send("evm_snapshot");
     });
 
+    it("Should throw an error when empty", async () => {
+        try {
+            await framework.batchCall([{} as any]).exec(alpha);
+        } catch (err: any) {
+            expect(err.type).to.equal("UNSUPPORTED_OPERATION");
+            expect(err.message).to.contain(
+                "The operation at index 0 is unsupported"
+            );
+        }
+    });
+
+    it("Should throw an error when data not provided", async () => {
+        try {
+            await framework
+                .batchCall([{ type: "ERC20_APPROVE" } as any])
+                .exec(alpha);
+        } catch (err: any) {
+            expect(err.message).to.contain(
+                "Cannot read properties of undefined"
+            );
+        }
+    });
+
+    it("Should throw an error when type not provided", async () => {
+        const transferOp = daix.transfer({
+            receiver: alpha.address,
+            amount: ethers.utils.parseUnits("1000").toString(),
+        });
+        try {
+            await framework
+                .batchCall([
+                    {
+                        populateTransactionPromise:
+                            transferOp.populateTransactionPromise,
+                    } as any,
+                ])
+                .exec(alpha);
+        } catch (err: any) {
+            expect(err.type).to.equal("UNSUPPORTED_OPERATION");
+            expect(err.message).to.contain(
+                "The operation at index 0 is unsupported"
+            );
+        }
+    });
+
     it("Should throw an error on unsupported operations", async () => {
-        const daix = await framework.loadSuperToken(superToken.address);
         const transferOp = daix.transfer({
             receiver: alpha.address,
             amount: ethers.utils.parseUnits("1000").toString(),
@@ -56,8 +102,31 @@ describe("Batch Call Tests", () => {
         }
     });
 
+    it("Should throw an error when amount not provided ERC20 approve", async () => {
+        const approveOp = daix.approve({ receiver: alpha.address } as any);
+        try {
+            await framework.batchCall([approveOp]).exec(alpha);
+        } catch (err: any) {
+            expect(err.message).to.contain("invalid BigNumber value");
+        }
+    });
+
+    it("Should throw an error when sender not provided ERC20 transferFrom", async () => {
+        const amount = ethers.utils.parseUnits("1000").toString();
+        const transferFromOp = daix.transferFrom({
+            receiver: alpha.address,
+            amount,
+        } as any);
+        try {
+            await framework.batchCall([transferFromOp]).exec(alpha);
+        } catch (err: any) {
+            expect(err.message).to.contain(
+                "a provider or signer is needed to resolve ENS names"
+            );
+        }
+    });
+
     it("Should throw an error if batch call fails", async () => {
-        const daix = await framework.loadSuperToken(superToken.address);
         const createFlowOp = daix.createFlow({
             sender: alpha.address,
             receiver: deployer.address,
@@ -66,15 +135,21 @@ describe("Batch Call Tests", () => {
         try {
             await framework.batchCall([createFlowOp]).exec(deployer);
         } catch (err: any) {
-            expect(err.message).to.contain(
-                "cannot estimate gas;"
-            );
+            expect(err.message).to.contain("cannot estimate gas;");
+        }
+    });
+
+    it("Should throw an error if amount not provided for upgrade", async () => {
+        const upgradeOp = daix.upgrade({} as any);
+        try {
+            await framework.batchCall([upgradeOp]).exec(deployer);
+        } catch (err: any) {
+            expect(err.message).to.contain("invalid BigNumber value");
         }
     });
 
     it("Should be able to create and execute a batch call (approve + transferFrom)", async () => {
         const amount = ethers.utils.parseUnits("1000").toString();
-        const daix = await framework.loadSuperToken(superToken.address);
         const approveOp = daix.approve({ receiver: alpha.address, amount });
         const transferFromOp = daix.transferFrom({
             sender: deployer.address,
@@ -89,7 +164,6 @@ describe("Batch Call Tests", () => {
 
     it("Should be able to batch create flows and batch update flows and batch delete flows", async () => {
         let flowRate = getPerSecondFlowRateByMonth("10000");
-        const daix = await framework.loadSuperToken(superToken.address);
         const createFlow1 = daix.createFlow({
             sender: charlie.address,
             receiver: alpha.address,
@@ -133,7 +207,4 @@ describe("Batch Call Tests", () => {
         await framework.batchCall([operation]).exec(deployer);
         expect(await superAppTester.val()).to.equal("69");
     });
-
-    // NOTE: this may be quite hard to test locally
-    // it.skip("Should be able to execute a forward batch call", async () => {});
 });

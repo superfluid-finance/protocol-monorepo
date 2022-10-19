b1dcd9cf4faf98088e77a6446e2b1c1f8602f781
Miao ZhiCheng <miao@superfluid.finance>
2022-10-19 17:43:33 +0300

    Yellowpaper 1 - Denotational Semantics of General Payment Primitives, and Its Payment System (#1105)

--- packages/spec-haskell/packages/core/src/Money/Systems/Superfluid/MonetaryUnitData/ConstantFlow.hs
+++ packages/spec-haskell/packages/core/src/Money/Systems/Superfluid/MonetaryUnitData/ConstantFlow.hs
@@ -13,22 +13,24 @@ import           Lens.Internal
 
 import           Money.Systems.Superfluid.SystemTypes
 
-class (Default amuLs, SuperfluidSystemTypes sft) => MonetaryUnitLenses amuLs sft | amuLs -> sft where
-    settledAt          :: Lens' amuLs (SFT_TS sft)
-    settledValue       :: Lens' amuLs (UntappedValue (SFT_MVAL sft))
-    netFlowRate        :: Lens' amuLs (SFT_MVAL sft)
+class ( Default amuLs
+      , SuperfluidSystemTypes sft
+      ) => MonetaryUnitLenses amuLs sft | amuLs -> sft where
+    settledAt    :: Lens' amuLs (SFT_TS sft)
+    settledValue :: Lens' amuLs (UntappedValue (SFT_MVAL sft))
+    netFlowRate  :: Lens' amuLs (SFT_MVAL sft)
 
 type MonetaryUnitData :: Type -> Type -> Type
 newtype MonetaryUnitData amuLs sft = MkMonetaryUnitData { getMonetaryUnitLenses :: amuLs } deriving (Default)
 
 instance MonetaryUnitLenses amuLs sft => Semigroup (MonetaryUnitData amuLs sft) where
-    (<>) (MkMonetaryUnitData a) (MkMonetaryUnitData b) =
+    MkMonetaryUnitData a <> MkMonetaryUnitData b =
         let t  = a^.settledAt
             t' = b^.settledAt
             settledΔ = MkUntappedValue $ a^.netFlowRate * fromIntegral (t' - t)
-            c = a & set  settledAt t'
-                  & over netFlowRate        (+ b^.netFlowRate)
-                  & over settledValue       (+ (b^.settledValue + settledΔ))
+            c = a & set  settledAt    t'
+                  & over netFlowRate  (+ b^.netFlowRate)
+                  & over settledValue (+ (b^.settledValue + settledΔ))
         in MkMonetaryUnitData c
 
 instance MonetaryUnitLenses amuLs sft => MonetaryUnitDataClass (MonetaryUnitData amuLs sft) sft where

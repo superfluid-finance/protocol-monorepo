e20e43495f734d8b1552df794d80b04105f95651
0xdavinchee <0xdavinchee@gmail.com>
2022-10-27 15:11:14 +0300

    send deployer some eth for deployERC1820

--- packages/ethereum-contracts/scripts/deploy-test-framework.js
+++ packages/ethereum-contracts/scripts/deploy-test-framework.js
@@ -7,6 +7,7 @@ const ERC1820Registry = require("./artifacts/ERC1820Registry.json");
 
 const ERC1820_ADDRESS = "0x1820a4b7618bde71dce8cdc73aab6c95905fad24";
 const ERC1820_BIN = ERC1820Registry.bin;
+const ERC1820_DEPLOYER = "0xa990077c3205cbDf861e17Fa532eeB069cE9fF96";
 const ERC1820_PAYLOAD =
     "0xf90a388085174876e800830c35008080b909e5" +
     ERC1820_BIN +
@@ -19,6 +20,15 @@ async function deployERC1820(provider) {
         "latest",
     ]);
     if (code === "0x") {
+        const [from] = await provider.send("eth_accounts", []);
+
+        await provider.send("eth_sendTransaction", [
+            {
+                from,
+                to: ERC1820_DEPLOYER,
+                value: "0x11c37937e080000",
+            },
+        ]);
         await provider.send("eth_sendRawTransaction", [ERC1820_PAYLOAD]);
 
         console.log("ERC1820 registry successfully deployed");

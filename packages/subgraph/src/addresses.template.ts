eb70ef26d5a92a8c55c5ac71ae1cbfd060ca4a44
0xdavinchee <0xdavinchee@gmail.com>
2022-11-14 12:14:10 +0200

    minor cleanups

--- packages/subgraph/src/addresses.template.ts
+++ packages/subgraph/src/addresses.template.ts
@@ -1,11 +1,9 @@
-import { Address, TypedMap } from "@graphprotocol/graph-ts";
+import { Address } from "@graphprotocol/graph-ts";
 
 // This file is a template file which is used for getting the address
 // based on the network we set in the set-network package.json file.
 // We add a bit of complexity to the package.json, but remove much
 // more as a result.
-// @note TODO: I am pretty sure we can remove {{network}} and just do
-// return Address.fromString("{{hostAddress}}")
 export function getHostAddress(): Address {
     return Address.fromString("{{hostAddress}}");
 }

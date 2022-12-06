1f84a8fea8ba57f9d98566ca5cae13b2fd29de38
0xdavinchee <0xdavinchee@gmail.com>
2022-12-06 14:31:46 +0200

    [SUBGRAPH] Add `gasUsed` property to all events (#1199)
    * subgraph gas used
    
    * bump subgraph package versions
    * add `gasUsed` property to all event entities
    * add tests for `gasUsed` property
    
    * bump subgraph version for retrigger
    
    * address review comments
    
    * remove casting
    * let it break if unable to get receipt
    
    * explicit throw + useful comment
    
    Co-authored-by: Miao ZhiCheng <miao@superfluid.finance>

--- packages/subgraph/src/utils.ts
+++ packages/subgraph/src/utils.ts
@@ -80,6 +80,17 @@ export function initializeEventEntity(
     entity.set("timestamp", Value.fromBigInt(event.block.timestamp));
     entity.set("transactionHash", Value.fromBytes(event.transaction.hash));
     entity.set("gasPrice", Value.fromBigInt(event.transaction.gasPrice));
+    const receipt = event.receipt;
+    if (receipt) {
+        entity.set("gasUsed", Value.fromBigInt(receipt.gasUsed));
+    } else {
+        // @note `gasUsed` is a non-nullable property in our `schema.graphql` file, so when we attempt to save 
+        // the entity with a null field, it will halt the subgraph indexing.
+        // Nonetheless, we explicitly throw if receipt is null, as this can arise due forgetting to include
+        // `receipt: true` under `eventHandlers` in our manifest (`subgraph.template.yaml`) file.
+        log.critical("receipt MUST NOT be null", []);
+    }
+
     return entity;
   }
 

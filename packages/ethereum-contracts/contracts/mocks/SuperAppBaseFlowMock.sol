b3ce08c6dbbaa13342f8da7c76e3186c7ba1db2f
sunnyjaycer <jesudason.joel@gmail.com>
2023-02-07 13:31:13 -0500

    bump solc to 0.8.16

--- packages/ethereum-contracts/contracts/mocks/SuperAppBaseFlowMock.sol
+++ packages/ethereum-contracts/contracts/mocks/SuperAppBaseFlowMock.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPLv3
-pragma solidity >=0.8.0;
+pragma solidity 0.8.16;
 
 import {ISuperfluid, ISuperToken, ISuperApp, SuperAppDefinitions} from "../interfaces/superfluid/ISuperfluid.sol";
 

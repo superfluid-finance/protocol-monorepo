150fa201e8018ceaffcaab06bdb834ddba5dcf37
Miao, ZhiCheng <miao@superfluid.finance>
2023-02-09 18:23:10 +0200

    flow2 implemented

--- packages/solidity-semantic-money/src/SemanticMoney.sol
+++ packages/solidity-semantic-money/src/SemanticMoney.sol
@@ -1,14 +1,63 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract SemanticMoney {
-    uint256 public number;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+type Time     is uint32;
+type Value    is int256;
+type FlowRate is int96;
+
+library MonetaryTypes {
+    function add(Time a, Time b) internal pure returns (Time) {
+        return Time.wrap(Time.unwrap(a) + Time.unwrap(b));
+    }
+    function sub(Time a, Time b) internal pure returns (Time) {
+        return Time.wrap(Time.unwrap(a) - Time.unwrap(b));
+    }
+    function add(Value a, Value b) internal pure returns (Value) {
+        return Value.wrap(Value.unwrap(a) + Value.unwrap(b));
+    }
+    function inv(FlowRate r) internal pure returns (FlowRate) {
+        return FlowRate.wrap(-FlowRate.unwrap(r));
+    }
+    function mul(FlowRate r, Time t) internal pure returns (Value) {
+        return Value.wrap(FlowRate.unwrap(r) * int(uint(Time.unwrap(t))));
+    }
+}
+
+struct BasicParticle {
+    Time     settled_at;
+    Value    settled_value;
+    FlowRate flow_rate;
+}
+
+library SemanticMoney {
+    using MonetaryTypes for Time;
+    using MonetaryTypes for Value;
+    using MonetaryTypes for FlowRate;
+    using SemanticMoney for BasicParticle;
+
+    function settle1(BasicParticle memory a, Time t) internal pure returns (BasicParticle memory b) {
+        b = a;
+        b.settled_value = rtb(a, t);
+        b.settled_at = t;
     }
 
-    function increment() public {
-        number++;
+    //
+    // Universal Index 1-primitives
+    //
+
+    function setFlow1(BasicParticle memory a, FlowRate r) internal pure returns (BasicParticle memory b) {
+        b = a;
+        b.flow_rate = r;
+    }
+
+    function rtb(BasicParticle memory a, Time t) internal pure returns (Value v) {
+        return a.flow_rate.mul(t.sub(a.settled_at)).add(a.settled_value);
+    }
+
+    function uu_flow2(BasicParticle memory a, BasicParticle memory b, FlowRate r, Time t)
+        internal pure returns (BasicParticle memory m, BasicParticle memory n) {
+        m = a.settle1(t).setFlow1(r);
+        n = b.settle1(t).setFlow1(r.inv());
     }
 }
--- packages/solidity-semantic-money/test/SemanticMoney.t.sol
+++ packages/solidity-semantic-money/test/SemanticMoney.t.sol
@@ -4,21 +4,52 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "../src/SemanticMoney.sol";
 
-contract CounterTest is Test {
-    SemanticMoney public counter;
+contract SemanticMoneyTest is Test {
+    using MonetaryTypes for Time;
+    using MonetaryTypes for Value;
+    using SemanticMoney for BasicParticle;
 
     function setUp() public {
-        counter = new SemanticMoney();
-        counter.setNumber(0);
     }
 
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+    function eqBasicParticle(BasicParticle memory a, BasicParticle memory b) internal pure returns (bool) {
+        return
+            Time.unwrap(a.settled_at) == Time.unwrap(b.settled_at) &&
+            Value.unwrap(a.settled_value) == Value.unwrap(b.settled_value) &&
+            FlowRate.unwrap(a.flow_rate) == FlowRate.unwrap(b.flow_rate);
     }
 
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function assumeSafeParticle(BasicParticle memory p) internal {
+        vm.assume(Time.unwrap(p.settled_at) < uint256(type(uint16).max));
+        vm.assume(Value.unwrap(p.settled_value) > 0);
+        vm.assume(Value.unwrap(p.settled_value) < int256(uint256(type(uint128).max)));
+    }
+
+    function testSettle1Twice(BasicParticle memory p, uint16 m) external {
+        assumeSafeParticle(p);
+        Time t1 = p.settled_at.add(Time.wrap(m));
+        BasicParticle memory p1 = p.settle1(t1);
+        BasicParticle memory p2 = p1.settle1(t1);
+        assertEq(Time.unwrap(p1.settled_at), Time.unwrap(t1));
+        assert(eqBasicParticle(p1, p2));
+    }
+
+    function testConstantRTB(BasicParticle memory p, uint16 m1, uint16 m2) external {
+        assumeSafeParticle(p);
+        Time t1 = p.settled_at.add(Time.wrap(m1));
+        Time t2 = t1.add(Time.wrap(m2));
+        assertEq(Value.unwrap(p.settle1(t1).rtb(t2)), Value.unwrap(p.settle1(t2).rtb(t2)));
+    }
+
+
+    function testFlow2Flow2(uint16 m1, int64 r1, uint16 m2, int64 r2, uint16 m3) external {
+        BasicParticle memory a;
+        BasicParticle memory b;
+        Time t1 = Time.wrap(m1);
+        Time t2 = t1.add(Time.wrap(m2));
+        Time t3 = t2.add(Time.wrap(m3));
+        (a, b) = a.uu_flow2(b, FlowRate.wrap(r1), t1);
+        (a, b) = a.uu_flow2(b, FlowRate.wrap(r2), t2);
+        assertEq(0, Value.unwrap(a.rtb(t3).add(b.rtb(t3))));
     }
 }

f1a96162185de188a759a4c04a8a30e984737a3f
Miao, ZhiCheng <miao@superfluid.finance>
2023-02-13 20:04:34 +0200

    added test-hot-fuzz.yml

--- flake.nix
+++ flake.nix
@@ -81,6 +81,8 @@
     };
     devShells.ci-spec-ghc925 = ci-spec "ghc925";
     devShells.ci-spec-ghc944 = ci-spec "ghc944";
+    devShells.ci-hot-fuzz = with pkgs; mkShell {
+        buildInputs = [ echidna ];
+    };
   });
 }
-

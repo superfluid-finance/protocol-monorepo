bd59a26688803ce535632eae4627b1ec6edf9e78
0xdavinchee <0xdavinchee@gmail.com>
2023-02-13 15:03:59 +0200

    duplicate keys makes no sense
    ty Miao

--- packages/hot-fuzz/index.js
+++ packages/hot-fuzz/index.js
@@ -16,20 +16,10 @@ function hotfuzzPatchTruffleConfig(c) {
             },
             "@superfluid-finance/ethereum-contracts/contracts/utils/SuperfluidFrameworkDeployer.sol": {
                 SuperfluidCFAv1DeployerLibrary: "0x" + "0".repeat(38) + "f2",
-            },
-            "@superfluid-finance/ethereum-contracts/contracts/utils/SuperfluidFrameworkDeployer.sol": {
                 SuperfluidGovDeployerLibrary: "0x" + "0".repeat(38) + "f3",
-            },
-            "@superfluid-finance/ethereum-contracts/contracts/utils/SuperfluidFrameworkDeployer.sol": {
                 SuperfluidHostDeployerLibrary: "0x" + "0".repeat(38) + "f4",
-            },
-            "@superfluid-finance/ethereum-contracts/contracts/utils/SuperfluidFrameworkDeployer.sol": {
                 SuperfluidIDAv1DeployerLibrary: "0x" + "0".repeat(38) + "f5",
-            },
-            "@superfluid-finance/ethereum-contracts/contracts/utils/SuperfluidFrameworkDeployer.sol": {
                 SuperfluidPeripheryDeployerLibrary: "0x" + "0".repeat(38) + "f6",
-            },
-            "@superfluid-finance/ethereum-contracts/contracts/utils/SuperfluidFrameworkDeployer.sol": {
                 SuperTokenDeployerLibrary: "0x" + "0".repeat(38) + "f7",
             },
         };

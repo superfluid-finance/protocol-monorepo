eb59999709b87c8fd227aa43c5092178944ad11f
0xdavinchee <0xdavinchee@gmail.com>
2023-03-03 15:41:24 +0200

    fix deploy-subgraph.yml

--- packages/subgraph/tasks/prepare-manifest.sh
+++ packages/subgraph/tasks/prepare-manifest.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-# args
-# $1 = the network
-
 mustache="../../node_modules/mustache/bin/mustache"
-npx mustache config/$1.json subgraph.template.yaml > subgraph.yaml
-npx mustache config/$1.json src/addresses.template.ts > src/addresses.ts
+
+# it is safe to hardcode matic here because this script is only used
+# for generating a "dummy" subgraph.yaml file and addresses.ts file
+# for the codegen step, it doesn't need correct addresses at that stage
+npx mustache config/matic.json subgraph.template.yaml > subgraph.yaml
+npx mustache config/matic.json src/addresses.template.ts > src/addresses.ts

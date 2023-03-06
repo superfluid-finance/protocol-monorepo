68f494905e757df135b3f346a27ebb649c26ac4f
0xdavinchee <0xdavinchee@gmail.com>
2023-03-06 10:32:59 +0200

    Update prepare-manifest.sh

--- packages/subgraph/tasks/prepare-manifest.sh
+++ packages/subgraph/tasks/prepare-manifest.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-mustache="../../node_modules/mustache/bin/mustache"
+# args
+# $1 = the network
 
-# it is safe to hardcode matic here because this script is only used
-# for generating a "dummy" subgraph.yaml file and addresses.ts file
-# for the codegen step, it doesn't need correct addresses at that stage
-npx mustache config/matic.json subgraph.template.yaml > subgraph.yaml
-npx mustache config/matic.json src/addresses.template.ts > src/addresses.ts
+mustache="../../node_modules/mustache/bin/mustache"
+NETWORK=$1
+
+if [ "$NETWORK" == "" ];then
+    NETWORK="matic"
+fi
+
+npx mustache config/$NETWORK.json subgraph.template.yaml > subgraph.yaml
+npx mustache config/$NETWORK.json src/addresses.template.ts > src/addresses.ts

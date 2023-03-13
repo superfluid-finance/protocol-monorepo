cf68d0ca37f36467427df5a48ff7b2a637060b68
0xdavinchee <0xdavinchee@gmail.com>
2023-03-13 15:45:53 +0200

    bump solc to 0.8.19

--- packages/ethereum-contracts/contracts/superfluid/SuperfluidToken.sol
+++ packages/ethereum-contracts/contracts/superfluid/SuperfluidToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPLv3
-pragma solidity 0.8.18;
+pragma solidity 0.8.19;
 
 import { ISuperfluid } from "../interfaces/superfluid/ISuperfluid.sol";
 import { ISuperAgreement } from "../interfaces/superfluid/ISuperAgreement.sol";

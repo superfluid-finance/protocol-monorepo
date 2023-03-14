8176312ec6117bf1482ca1135ce8c0b1beb1c3f6
didi <git@d10r.net>
2023-03-14 20:58:52 +0100

    don't use lib in foundry test

--- packages/ethereum-contracts/test/foundry/apps/SuperAppBaseFlow.t.sol
+++ packages/ethereum-contracts/test/foundry/apps/SuperAppBaseFlow.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPLv3
-pragma solidity 0.8.16;
+pragma solidity 0.8.19;
 
 import "forge-std/console.sol";
 import "../FoundrySuperfluidTester.sol";
@@ -7,13 +7,8 @@ import { SuperAppBaseFlow } from "@superfluid-finance/ethereum-contracts/contrac
 import { SuperAppBaseFlowTester } from "@superfluid-finance/ethereum-contracts/contracts/mocks/SuperAppBaseFlowTester.sol";
 import { ISuperToken } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
 import { IConstantFlowAgreementV1 } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IConstantFlowAgreementV1.sol";
-import { SuperTokenV1Library } from "@superfluid-finance/ethereum-contracts/contracts/apps/SuperTokenV1Library.sol";
 
 contract SuperAppBaseFlowTest is FoundrySuperfluidTester {
-    using CFAv1Library for CFAv1Library.InitData;
-    using SuperTokenV1Library for ISuperToken;
-    using SuperTokenV1Library for SuperToken;
-
     SuperAppBaseFlowTester superApp;
     ISuperToken otherSuperToken;
 
@@ -24,7 +19,8 @@ contract SuperAppBaseFlowTest is FoundrySuperfluidTester {
         vm.startPrank(admin);
         superApp = new SuperAppBaseFlowTester(sf.host);
         superApp.setAcceptedSuperToken(superToken, true);
-        (, otherSuperToken) = sfDeployer.deployWrapperSuperToken("FTT", "FTT", 18, type(uint256).max);
+        otherSuperToken = sfDeployer.deployPureSuperToken("FTT", "FTT", 1e27);
+        otherSuperToken.transfer(alice, 1e21);
         vm.stopPrank();
     }
 
@@ -37,15 +33,29 @@ contract SuperAppBaseFlowTest is FoundrySuperfluidTester {
 
     function testFlowToSuperApp() public {
         vm.startPrank(alice);
-        superToken.createFlow(address(superApp), int96(69));
+        sf.host.callAgreement(
+            sf.cfa,
+            abi.encodeCall(
+                sf.cfa.createFlow,
+                (superToken, address(superApp), int96(69), new bytes(0))
+            ),
+            new bytes(0) // userData
+        );
         vm.stopPrank();
     }
 
     function testFlowOfNotAcceptedSuperTokenToSuperApp() public {
         vm.startPrank(alice);
         vm.expectRevert();
-        //vm.expectRevert(SuperAppBaseFlow.UnauthorizedHost.selector);
-        otherSuperToken.createFlow(address(superApp), int96(69));
+        //vm.expectRevert(SuperAppBaseFlow.NotAcceptedSuperToken.selector);
+        sf.host.callAgreement(
+            sf.cfa,
+            abi.encodeCall(
+                sf.cfa.createFlow,
+                (otherSuperToken, address(superApp), int96(69), new bytes(0))
+            ),
+            new bytes(0) // userData
+        );
         vm.stopPrank();
     }
 }

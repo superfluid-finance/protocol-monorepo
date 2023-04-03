ea8c3df282221402ca474e232c8271888c8a3bfc
Miao, ZhiCheng <miao@superfluid.finance>
2023-04-03 17:58:21 +0300

    fix tasks/check-changeset.sh

--- tasks/shellcheck-all-tasks.sh
+++ tasks/shellcheck-all-tasks.sh
@@ -7,5 +7,5 @@ SHELLCHECK="npx shellcheck"
 cd "$(dirname "$0")"/.. || exit 1
 
 # shellcheck disable=SC2086
-find tasks packages/*/tasks packages/*/*/tasks -name '*.sh' -print0 \
+find tasks packages/*/tasks -name '*.sh' -print0 \
     | xargs --null -- $SHELLCHECK -s bash

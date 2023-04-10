640626aa4c739e2c5d3328bbe4d8f81d34d26727
0xdavinchee <0xdavinchee@gmail.com>
2023-04-10 17:09:32 +0300

    it likes the skipFiles
    no extract step + add back skipFiles

--- packages/ethereum-contracts/.solcover.js
+++ packages/ethereum-contracts/.solcover.js
@@ -4,6 +4,21 @@ module.exports = {
     providerOptions: {
         network_id: 8555,
     },
+    skipFiles: [
+        "mocks/",
+        // we skip the coverage for the SuperAppBase contracts because
+        // we override the functions in child contracts
+        "apps/SuperAppBase.sol",
+        "apps/SuperAppBaseCFA.sol",
+
+        // we skip the coverage for these contracts because they are
+        // only used for testing
+        "utils/SuperfluidFrameworkDeployer",
+        "utils/SuperTokenDeployer",
+        "utils/TestToken",
+        "utils/TestResolver",
+        "utils/TestGovernance",
+    ],
     mocha: {
         grep: "@skip-on-coverage", // Find everything with this tag
         invert: true, // Run the grep's inverse set.

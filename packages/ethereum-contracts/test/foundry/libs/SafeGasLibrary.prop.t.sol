9e69860a1a19af9c152713ae7e35952b55b8df57
0xdavinchee <0xdavinchee@gmail.com>
2023-04-10 14:09:42 +0300

    cleanup
    - add back extraction
    - move ignore list to codecov.yml
    - remove skipFiles from solcover.js
    - add test for SF_GOV_INVALID_LIQUIDATION_OR_PATRICIAN_PERIOD
    - add safegas prop test
    - try out separate upload to allow codecov to merge on their end

---
+++ packages/ethereum-contracts/test/foundry/libs/SafeGasLibrary.prop.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: AGPLv3
+pragma solidity 0.8.19;
+
+import { SafeGasLibrary } from "@superfluid-finance/ethereum-contracts/contracts/libs/SafeGasLibrary.sol";
+
+import "forge-std/Test.sol";
+
+/// @title SafeGasLibraryProperties
+/// @author Superfluid
+/// @notice This contract tests that the SafeGasLibrary behaves as expected.
+/// @dev If the gasleft() inside the function is less than 1/63 of the gasleft() before the function call,
+/// then the function ran out of gas and we revert the whole transaction.
+/// We mock this here by using a multiple of the gasleft() before the function call.
+contract SafeGasLibraryProperties is Test {
+
+    function test_SafeGasRevertsAsExpected(uint16 multiple) public {
+        if (multiple > 63) {
+            vm.expectRevert(SafeGasLibrary.OUT_OF_GAS.selector);
+        }
+        uint256 gasLeft = gasleft();
+        SafeGasLibrary._revertWhenOutOfGas(gasLeft * multiple);
+    }
+
+}

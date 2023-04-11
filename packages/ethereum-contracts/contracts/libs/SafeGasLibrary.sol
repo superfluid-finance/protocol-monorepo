5b793a67f413491b916606626cea86e7dc93fa60
0xdavinchee <0xdavinchee@gmail.com>
2023-04-11 13:49:51 +0300

    out of gas consistency

--- packages/ethereum-contracts/contracts/libs/SafeGasLibrary.sol
+++ packages/ethereum-contracts/contracts/libs/SafeGasLibrary.sol
@@ -5,7 +5,11 @@ pragma solidity 0.8.19;
 /// @author Superfluid
 /// @notice An internal library used to handle out of gas errors
 library SafeGasLibrary {
-    error OUT_OF_GAS();
+    error OUT_OF_GAS(); // 0x20afada5
+
+    function _isOutOfGas(uint256 gasLeftBefore) internal view returns (bool) {
+        return gasleft() <= gasLeftBefore / 63;
+    }
 
     /// @dev A function used in the catch block to handle true out of gas errors
     /// @param gasLeftBefore the gas left before the try/catch block
@@ -13,7 +17,7 @@ library SafeGasLibrary {
 // If the function actually runs out of gas, not just hitting the safety gas limit, we revert the whole transaction.
 // This solves an issue where the gas estimaton didn't provide enough gas by default for the function to succeed.
 // See https://medium.com/@wighawag/ethereum-the-concept-of-gas-and-its-dangers-28d0eb809bb2
-        if (gasleft() <= gasLeftBefore / 63) {
+        if (_isOutOfGas(gasLeftBefore)) {
             revert OUT_OF_GAS();
         }
     }

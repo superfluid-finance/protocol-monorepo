0f6db31a6f6f5819c8b20e0289a1059ae1dbc406
0xdavinchee <0xdavinchee@gmail.com>
2023-04-20 18:29:58 +0300

    pure functions tested

--- packages/ethereum-contracts/test/foundry/agreements/GeneralDistributionAgreementV1.prop.sol
+++ packages/ethereum-contracts/test/foundry/agreements/GeneralDistributionAgreementV1.prop.sol
@@ -13,59 +13,73 @@ import {
 } from "../../../contracts/agreements/GeneralDistributionAgreementV1.sol";
 
 contract GeneralDistributionAgreementV1Mock is GeneralDistributionAgreementV1 {
-    constructor()
-        GeneralDistributionAgreementV1(
-            ISuperfluid(address(0))
-        )
-    {}
+    constructor() GeneralDistributionAgreementV1(ISuperfluid(address(0))) {}
 }
 
 contract GeneralDistributionAgreementV1Properties is
     GeneralDistributionAgreementV1Mock,
     Test
 {
-    function test_Universal_Index_Data_Encode_Decode(
+    function test_Particle_Input_Encode_Decode_Universal_Index_Data(
         int96 flowRate,
         uint32 settledAt,
         int256 settledValue,
-        uint96 buffer,
+        uint96 totalBuffer,
         bool isPool
     ) public {
-        vm.assume(flowRate >= 0);
-        vm.assume(settledAt > 0);
-        vm.assume(settledValue > 0);
-        BasicParticle memory ogParticle = BasicParticle({
+        BasicParticle memory particle = BasicParticle({
             _flow_rate: FlowRate.wrap(flowRate),
             _settled_at: Time.wrap(settledAt),
             _settled_value: Value.wrap(settledValue)
         });
-        bytes32[] memory encodedData = _encodeUniversalIndexData(ogParticle, buffer, isPool);
+        bytes32[] memory encoded = _encodeUniversalIndexData(
+            particle,
+            totalBuffer,
+            isPool
+        );
         (, UniversalIndexData memory decoded) = _decodeUniversalIndexData(
-            encodedData
+            encoded
         );
 
-        assertEq(
-            FlowRate.unwrap(ogParticle._flow_rate),
-            decoded.flowRate,
-            "flow rate mismatch"
+        assertEq(flowRate, decoded.flowRate, "flowRate not equal");
+        assertEq(settledAt, decoded.settledAt, "settledAt not equal");
+        assertEq(settledValue, decoded.settledValue, "settledValue not equal");
+        assertEq(totalBuffer, decoded.totalBuffer, "totalBuffer not equal");
+        assertEq(isPool, decoded.isPool, "isPool not equal");
+    }
+
+    function test_UIData_Input_Encode_Decode_Universal_Index_Data(
+        int96 flowRate,
+        uint32 settledAt,
+        int256 settledValue,
+        uint96 totalBuffer,
+        bool isPool
+    ) public {
+        UniversalIndexData memory data = UniversalIndexData({
+            flowRate: flowRate,
+            settledAt: settledAt,
+            settledValue: settledValue,
+            totalBuffer: totalBuffer,
+            isPool: isPool
+        });
+
+        bytes32[] memory encoded = _encodeUniversalIndexData(data);
+        (, UniversalIndexData memory decoded) = _decodeUniversalIndexData(
+            encoded
         );
-        assertEq(
-            Time.unwrap(ogParticle._settled_at),
-            decoded.settledAt,
-            "settled at mismatch"
-        );
-        assertEq(
-            buffer, decoded.totalBuffer,
-            "total buffer mismatch"
-        );
-        assertEq(
-            Value.unwrap(ogParticle._settled_value),
-            decoded.settledValue,
-            "settled value mismatch"
-        );
-        assertEq(
-            isPool, decoded.isPool
-        );
+
+        assertEq(flowRate, decoded.flowRate, "flowRate not equal");
+        assertEq(settledAt, decoded.settledAt, "settledAt not equal");
+        assertEq(settledValue, decoded.settledValue, "settledValue not equal");
+        assertEq(totalBuffer, decoded.totalBuffer, "totalBuffer not equal");
+        assertEq(isPool, decoded.isPool, "isPool not equal");
+    }
+
+    function test_Get_Basic_Particle_From_UIndex(UniversalIndexData memory data) public {
+        BasicParticle memory particle = _getBasicParticleFromUIndex(data);
+        assertEq(data.flowRate, int96(FlowRate.unwrap(particle._flow_rate)), "flowRate not equal");
+        assertEq(data.settledAt, Time.unwrap(particle._settled_at), "settledAt not equal");
+        assertEq(data.settledValue, Value.unwrap(particle._settled_value), "settledValue not equal");
     }
 
     function test_Flow_Distribution_Data_Encode_Decode(
@@ -76,16 +90,20 @@ contract GeneralDistributionAgreementV1Properties is
         vm.assume(buffer >= 0);
         GeneralDistributionAgreementV1.FlowDistributionData
             memory original = GeneralDistributionAgreementV1
-                .FlowDistributionData({ flowRate: flowRate, lastUpdated: uint32(block.timestamp), buffer: buffer });
-        bytes32[] memory encodedData = _encodeFlowDistributionData(original);
+                .FlowDistributionData({
+                    flowRate: flowRate,
+                    lastUpdated: uint32(block.timestamp),
+                    buffer: buffer
+                });
+        bytes32[] memory encoded = _encodeFlowDistributionData(original);
         (
             ,
             GeneralDistributionAgreementV1.FlowDistributionData memory decoded
-        ) = _decodeFlowDistributionData(uint256(encodedData[0]));
+        ) = _decodeFlowDistributionData(uint256(encoded[0]));
 
-        assertEq(original.flowRate, decoded.flowRate);
-        assertEq(original.buffer, decoded.buffer);
-        assertEq(original.lastUpdated, decoded.lastUpdated);
+        assertEq(original.flowRate, decoded.flowRate, "flowRate not equal");
+        assertEq(original.buffer, decoded.buffer, "buffer not equal");
+        assertEq(original.lastUpdated, decoded.lastUpdated, "lastUpdated not equal");
     }
 
     function test_Pool_Member_Data_Encode_Decode(
@@ -98,13 +116,13 @@ contract GeneralDistributionAgreementV1Properties is
                 pool: pool,
                 poolId: poolId
             });
-        bytes32[] memory encodedData = _encodePoolMemberData(original);
+        bytes32[] memory encoded = _encodePoolMemberData(original);
         (
             ,
             GeneralDistributionAgreementV1.PoolMemberData memory decoded
-        ) = _decodePoolMemberData(uint256(encodedData[0]));
+        ) = _decodePoolMemberData(uint256(encoded[0]));
 
-        assertEq(original.pool, decoded.pool);
-        assertEq(original.poolId, decoded.poolId);
+        assertEq(original.pool, decoded.pool, "pool not equal");
+        assertEq(original.poolId, decoded.poolId, "poolId not equal");
     }
 }

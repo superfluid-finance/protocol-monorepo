7a488f0203e4e9c906e7e6092d9024ed72c5c854
didi <git@d10r.net>
2023-04-25 20:59:17 +0200

    use Nix provided shellcheck instead of allowing implicit downloads

--- tasks/shellcheck-all-tasks.sh
+++ tasks/shellcheck-all-tasks.sh
@@ -2,10 +2,15 @@
 
 set -xe
 
-SHELLCHECK="npx shellcheck"
+# in a Nix shell, shellcheck is in PATH, else use the npm package
+if command -v shellcheck > /dev/null 2>&1; then
+  cd "$(dirname "$0")"/.. || exit 1
 
-cd "$(dirname "$0")"/.. || exit 1
+  # shellcheck disable=SC2086
+  find tasks packages/*/tasks -name '*.sh' -print0 \
+      | xargs -0 -- shellcheck -s bash
+else
+  echo "WARNING: shellcheck is not installed, skipping"
+fi
 
-# shellcheck disable=SC2086
-find tasks packages/*/tasks -name '*.sh' -print0 \
-    | xargs -0 -- $SHELLCHECK -s bash
+

fc2e956416ca25aca880313ade316cac6da3d07a
Miao, ZhiCheng <miao@superfluid.finance>
2023-05-03 22:32:04 +0300

    added docs/aqueduct-remainder-flow-adjustment.png

--- packages/solidity-semantic-money/src/examples/Aqueduct.sol
+++ packages/solidity-semantic-money/src/examples/Aqueduct.sol
@@ -84,49 +84,61 @@ contract Aqueduct {
 
     function _onFlowUpdate(Side storage a, Side storage b, address from, FlowRate ir0, FlowRate ir1) internal {
         AqueductLibrary.TraderSideState memory tssB;
-        FlowRate drr;
+        FlowRate drr; // distribution rate requested
         (a.state, tssB, drr) = AqueductLibrary.updateSide(a.state, ir0, ir1);
-
         {
-            FlowRate ar0 = a.token.getFlowRate(address(this), from, ADJUSTMENT_FLOW_ID);
-            FlowRate dr0 = a.pool.getConnectedFlowRate();
+            FlowRate dr0 = a.pool.getDistributionFlowRate();
             (,,FlowRate dr1) = a.token.distributeFlow(address(this), a.pool, SWAP_DISTRIBUTE_FLOW_ID, drr);
-            _adjustFlowRemainder(a, from, ir1 - ir0, dr1, ar0, dr0);
+            _adjustFlowRemainder(a, from, ir1 - ir0, dr1 - dr0);
         }
-
         {
+            // cancel existing remainder flow
             FlowRate ar0 = b.token.getFlowRate(address(this), from, ADJUSTMENT_FLOW_ID);
-            FlowRate dr0 = b.pool.getConnectedFlowRate();
+            FlowRate dr0 = b.pool.getDistributionFlowRate();
             b.pool.updateMember(from, tssB.nUnits);
             (,,FlowRate dr1) = b.token.distributeFlow(address(this), b.pool, SWAP_DISTRIBUTE_FLOW_ID, dr0 + ar0);
-            _adjustFlowRemainder(b, from, FlowRate.wrap(0), dr1, ar0, dr0);
+            _adjustFlowRemainder(b, from, FlowRate.wrap(0), dr1 - dr0);
         }
     }
 
     /**
      * @dev Helper function for adjusting flow remainder for the new receiver
      *
-     * @param ird - Input rate difference
-     * @param dr1 - ??
+     * @param a - side of the pool to be adjusted,
+     * @param newRemFlowReceiver - new (maybe) remainder flow receiver.
+     * @param ird - input rate delta.
+     * @param drd - flow distribution rate delta before/after flow distribution update.
      */
     function _adjustFlowRemainder(Side storage a, address newRemFlowReceiver,
-                                  FlowRate ird, FlowRate dr1,
-                                  FlowRate ar0, FlowRate dr0) internal {
+                                  FlowRate ird, FlowRate drd) internal {
+        // * Given definitions:
+        //
+        // 1) ird, drd defined in parameters. Additionally,
+        // 2) br0: flow rate to the current remainder flow receiver.
+        // 3) crd: the control flow rate delta. Control flow is flow from aqueduct to the pool itself.
+        // 4) jrd: pool adjustment flow rate delta.
+        // 5) ar0: current remainder flow rate to the new remainder receiver.
+        // 6) ar1: new remainder flow rate to the new remainder receiver.
+        //
+        // * We have:
+        //
+        // a) when changing remainder flow receiver:
+        //      ird + jrd = ar1 - ar0 - br0 + crd && ar0 == 0
+        //    otherwise:
+        //      ird + jrd = ar1 - br0 + crd
+        //    so that net flow rate to aqueduct is always 0
+        // b) crd = drd + jrd
+        //    so that netflow rate to the pool is always 0
+        //
+        // * Then the solution to ar1 is always:
+        //
+        // c) ar1 = ird - drd + br0
+        FlowRate br0 = a.token.getFlowRate(address(this), a.remFlowReceiver, ADJUSTMENT_FLOW_ID);
+        FlowRate ar1 = ird - drd + br0;
         if (a.remFlowReceiver != newRemFlowReceiver) {
-            FlowRate br0 = a.token.getFlowRate(address(this), a.remFlowReceiver, ADJUSTMENT_FLOW_ID);
-            // solve ar1 in: ird = (dr1 - dr0) + (ar1 - ar0) + (0 - br0)
-            // =>
-            FlowRate ar1 = ird + dr0 - dr1 + ar0 + br0;
-
             a.token.flow(address(this), a.remFlowReceiver, ADJUSTMENT_FLOW_ID, FlowRate.wrap(0));
-            a.token.flow(address(this), newRemFlowReceiver, ADJUSTMENT_FLOW_ID, ar1);
             a.remFlowReceiver = newRemFlowReceiver;
-        } else {
-            // solve ar1 in: ird = (dr1 - dr0) + (ar1 - ar0)
-            // =>
-            FlowRate ar1 = ird + dr0 - dr1 + ar0;
-
-            a.token.flow(address(this), newRemFlowReceiver, ADJUSTMENT_FLOW_ID, ar1);
         }
+        a.token.flow(address(this), newRemFlowReceiver, ADJUSTMENT_FLOW_ID, ar1);
     }
 }

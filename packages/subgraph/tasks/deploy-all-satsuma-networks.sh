9d61624b022dc92cb6bc0416bc1a8fc11a688a85
0xdavinchee <0xdavinchee@gmail.com>
2023-05-10 15:17:29 +0300

    sepolia additions
    - update metadata
    - fix constants.ts
    - add eth-sepolia to satsuma

--- packages/subgraph/tasks/deploy-all-satsuma-networks.sh
+++ packages/subgraph/tasks/deploy-all-satsuma-networks.sh
@@ -4,7 +4,7 @@
 # $1 = the version label
 
 # TODO: use jq and get the networks from metadata.json networks file
-SATSUMA_NETWORKS=( "matic" "xdai" "eth-mainnet" )
+SATSUMA_NETWORKS=( "matic" "xdai" "eth-mainnet" "eth-sepolia" )
 
 for i in "${SATSUMA_NETWORKS[@]}";do
     ./tasks/deploy-to-satsuma-network.sh "$1" "$i"

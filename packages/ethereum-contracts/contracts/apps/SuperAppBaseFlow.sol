bda49346a414c411a3a23cb8316a29d09a0e8dee
Miao, ZhiCheng <zhicheng.miao@gmail.com>
2023-06-26 12:54:13 +0300

    update licenses for individual files

--- packages/ethereum-contracts/contracts/apps/SuperAppBaseFlow.sol
+++ packages/ethereum-contracts/contracts/apps/SuperAppBaseFlow.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: AGPLv3
+// SPDX-License-Identifier: MIT
 pragma solidity >= 0.8.0;
 
 import { ISuperfluid, ISuperToken, ISuperApp, SuperAppDefinitions } from "../interfaces/superfluid/ISuperfluid.sol";

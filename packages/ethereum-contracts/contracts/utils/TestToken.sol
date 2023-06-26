273bfdb2921e99111bd099f40c311643663adbb5
Miao, ZhiCheng <zhicheng.miao@gmail.com>
2023-06-26 14:22:37 +0300

    fixes

--- packages/ethereum-contracts/contracts/utils/TestToken.sol
+++ packages/ethereum-contracts/contracts/utils/TestToken.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: AGPLv3
-pragma solidity 0.8.19;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.4;
 
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 

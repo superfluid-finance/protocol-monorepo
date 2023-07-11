801d940ca4ba9169a1f01acba4ca41fe7e9ea158
Miao, ZhiCheng <zhicheng.miao@gmail.com>
2023-07-11 11:32:39 +0300

    refactor check-package-version more

--- tasks/check-package-version.sh
+++ tasks/check-package-version.sh
@@ -1,16 +1,26 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # This script checks if the package version has changed since the last release.
-# If the version has changed, it will set the $package_name=1 
+# Usage: check-package-version.sh [package_name] [do_publish_var_name] [new_version_var_name]
+#
+# It will print these to standard output:
+# ${do_publish_var_name}=0|1
+# ${new_version_var_name}=x.y.z # only when ${do_publish_var_name}=1
+
 package_name=$1
-published_version=$(npm show @superfluid-finance/"$package_name"@latest version)
+do_publish_var_name=$2
+new_version_var_name=$3
+if ! published_version=$(npm show @superfluid-finance/"$package_name"@latest version); then
+    echo "Querying current published version failed." >&2
+    exit 1
+fi
 new_version=$(jq -r .version packages/"$package_name"/package.json)
 
 if [ "$published_version" != "$new_version" ]; then
-    echo "$package_name changed: $published_version -> $new_version"
-    echo "$package_name=1" >> "$GITHUB_ENV"
+    echo "$package_name changed: $published_version -> $new_version" >&2
+    echo "$do_publish_var_name=1"
+    echo "$new_version_var_name=$new_version"
 else
-    echo "$package_name unchanged: $published_version"
+    echo "$package_name unchanged: $published_version" >&2
+    echo "$do_publish_var_name=0"
 fi
-
-echo "$package_name=$new_version" >> "$GITHUB_OUTPUT"

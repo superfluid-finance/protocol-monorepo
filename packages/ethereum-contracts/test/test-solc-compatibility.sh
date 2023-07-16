40ac4433b368ec6f6d44e3b8d9fa397675fed82a
Miao, ZhiCheng <zhicheng.miao@gmail.com>
2023-07-16 19:19:14 +0300

    fix packages/ethereum-contracts/test/test-solc-compatibility.sh

--- packages/ethereum-contracts/test/test-solc-compatibility.sh
+++ packages/ethereum-contracts/test/test-solc-compatibility.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # NOTE: This utility checks the solc-0.8.4 compatibility of all the interface contracts we export
 
@@ -7,18 +7,21 @@ set -xe
 
 cd "$(dirname "$0")"/..
 
-[ -z "$(apt list --installed | grep coreutils)" ] && apt-get install coreutils
-
 # Download solc if needed and verify its checksum
-mkdir -p ./build/bin
-SOLC=build/bin/solc-0.8.4
-if [ ! -f "$SOLC" ]; then
-    wget https://github.com/ethereum/solc-bin/raw/gh-pages/linux-amd64/solc-linux-amd64-v0.8.4%2Bcommit.c7e474f2 -O $SOLC
-    chmod +x ./$SOLC
+SOLC=solc-0.8.4
+if ! which $SOLC; then
+  # This process assuming ubuntu Linux.
+  [ -z "$(apt list --installed | grep coreutils)" ] && apt-get install coreutils
+  mkdir -p ./build/bin
+  SOLC=build/bin/solc-0.8.4
+  if [ ! -f "$SOLC" ]; then
+      wget https://github.com/ethereum/solc-bin/raw/gh-pages/linux-amd64/solc-linux-amd64-v0.8.4%2Bcommit.c7e474f2 -O $SOLC
+      chmod +x ./$SOLC
+  fi
+  CHKSUM=$(sha256sum ./$SOLC | awk '{print $1}')
+  EXPECTED_CHKSUM="f7115ccaf11899dcf3aaa888949f8614421f2d10af65a74870bcfd67010da7f8"
+  [ "$CHKSUM" == "$EXPECTED_CHKSUM" ]
 fi
-CHKSUM=$(sha256sum ./$SOLC | awk '{print $1}')
-EXPECTED_CHKSUM="f7115ccaf11899dcf3aaa888949f8614421f2d10af65a74870bcfd67010da7f8"
-[ "$CHKSUM" == "$EXPECTED_CHKSUM" ]
 
 # https://github.com/ethereum/solc-bin/blob/gh-pages/linux-amd64/list.json
 # 0x64016310a57caf1af76a3610f1f94c8848c04c9673e7fa268492e608918a4bdc
@@ -30,13 +33,13 @@ EXPECTED_CHKSUM="f7115ccaf11899dcf3aaa888949f8614421f2d10af65a74870bcfd67010da7f
 ln -sf ../../node_modules/@openzeppelin .
 
 # verify they are compatible with the minimum version of the SOLC we support
-find contracts/interfaces/ -name '*.sol' | while read i;do
-  echo $i
-  $SOLC --allow-paths . $i
-done
+find contracts/{interfaces/,apps/} -name '*.sol' | while read i;do
+  # TODO: these use abi.encodeAbi, which is only available from 0.8.11
+  [ "$i" == contracts/apps/SuperAppBaseFlow.sol ] && continue;
+  [ "$i" == contracts/apps/SuperTokenV1Library.sol ] && continue;
+  [ "$i" == contracts/apps/CFAv1Library.sol ] && continue;
+  [ "$i" == contracts/apps/IDAv1Library.sol ] && continue;
 
-find contracts/apps/ -name '*.sol' | while read i;do
-  echo $i
   $SOLC --allow-paths . $i
 done
 

8588461d8cac9674edbd81117fcc578cfb7fbfc5
0xdavinchee <0xdavinchee@gmail.com>
2023-07-21 10:59:30 +0200

    bump semantic money versions

--- packages/solidity-semantic-money/src/ref-impl/ISuperfluidPool.sol
+++ packages/solidity-semantic-money/src/ref-impl/ISuperfluidPool.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >= 0.8.4;
+pragma solidity >= 0.8.11;
 
 import {
     Time, Value, FlowRate, Unit,
--- packages/solidity-semantic-money/test/SemanticMoney.t.sol
+++ packages/solidity-semantic-money/test/SemanticMoney.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.19;
+pragma solidity 0.8.21;
 
 import "forge-std/Test.sol";
 import "../src/SemanticMoney.sol";

8ae94a855b0a77c88082f96161e5844604c7705d
Miao, ZhiCheng <zhicheng.miao@gmail.com>
2023-07-24 22:03:13 +0300

    echidna fixes wip

--- packages/hot-fuzz/index.js
+++ packages/hot-fuzz/index.js
@@ -1,45 +1,42 @@
-function addr(a) { "0x" + "0".repeat(40 - a.length) + a; }
+function addr(a) { return "0x" + "0".repeat(40 - a.length) + a; }
 
 function hotfuzzPatchTruffleConfig(c) {
     if (process.env.HOT_FUZZ_MODE) {
-
-        // use different build directory in order to not interfere with regular builds
-        c.contracts_build_directory = "./build/hot-fuzz-contracts";
-
-        // create settings field if not exists
-        c.compilers.solc.settings = {
-            ...c.compilers.solc.settings,
-        };
-
-        // Manual library mappings, these should match deployContracts in "echidna.yaml".
-        c.compilers.solc.settings.libraries = {
-            ...c.compilers.solc.settings.libraries,
-            "@superfluid-finance/ethereum-contracts/contracts/libs/SlotsBitmapLibrary.sol": {
-                SlotsBitmapLibrary: addr("f01"),
-            },
-            // to generate:
-            // $ (j=0;sed -nE 's/^library\s+(\w+)\s+\{/\1/pg' contracts/utils/SuperfluidFrameworkDeploymentSteps.sol | sort | while read i;do echo "$i: addr(\"f1$(printf "%x" $j)\")";j=$((j+1));done)
-            "@superfluid-finance/ethereum-contracts/contracts/utils/SuperfluidFrameworkDeploymentSteps.sol": {
-                CFAv1ForwarderDeployerLibrary: addr("f10"),
-                IDAv1ForwarderDeployerLibrary: addr("f11"),
-                ProxyDeployerLibrary: addr("f12"),
-                SuperfluidCFAv1DeployerLibrary: addr("f13"),
-                SuperfluidGovDeployerLibrary: addr("f14"),
-                SuperfluidHostDeployerLibrary: addr("f15"),
-                SuperfluidIDAv1DeployerLibrary: addr("f16"),
-                SuperfluidLoaderDeployerLibrary: addr("f17"),
-                SuperfluidNFTLogicDeployerLibrary: addr("f18"),
-                SuperfluidPeripheryDeployerLibrary: addr("f19"),
-                SuperTokenDeployerLibrary: addr("f1a"),
-                TokenDeployerLibrary: addr("f1b"),
-            },
-        };
-
-        c.compilers.solc.settings.optimizer = {
-            enabled: true,
-            runs: 200,
-        };
-
+//
+//        // create settings field if not exists
+//        c.compilers.solc.settings = {
+//            ...c.compilers.solc.settings,
+//        };
+//
+//        // Manual library mappings, these should match deployContracts in "echidna.yaml".
+//        c.compilers.solc.settings.libraries = {
+//            ...c.compilers.solc.settings.libraries,
+//            "@superfluid-finance/ethereum-contracts/contracts/libs/SlotsBitmapLibrary.sol": {
+//                SlotsBitmapLibrary: addr("f01"),
+//            },
+//            // to generate:
+//            // $ (j=0;sed -nE 's/^library\s+(\w+)\s+\{/\1/pg' contracts/utils/SuperfluidFrameworkDeploymentSteps.sol | sort | while read i;do echo "$i: addr(\"f1$(printf "%x" $j)\")";j=$((j+1));done)
+//            "@superfluid-finance/ethereum-contracts/contracts/utils/SuperfluidFrameworkDeploymentSteps.sol": {
+//                CFAv1ForwarderDeployerLibrary: addr("f10"),
+//                IDAv1ForwarderDeployerLibrary: addr("f11"),
+//                ProxyDeployerLibrary: addr("f12"),
+//                SuperfluidCFAv1DeployerLibrary: addr("f13"),
+//                SuperfluidGovDeployerLibrary: addr("f14"),
+//                SuperfluidHostDeployerLibrary: addr("f15"),
+//                SuperfluidIDAv1DeployerLibrary: addr("f16"),
+//                SuperfluidLoaderDeployerLibrary: addr("f17"),
+//                SuperfluidNFTLogicDeployerLibrary: addr("f18"),
+//                SuperfluidPeripheryDeployerLibrary: addr("f19"),
+//                SuperTokenDeployerLibrary: addr("f1a"),
+//                TokenDeployerLibrary: addr("f1b"),
+//            },
+//            "@superfluid-finance/ethereum-contracts/contracts/apps/SuperfluidLoaderLibrary.sol": {
+//                SuperfluidLoaderLibrary: addr("f20")
+//            },
+//            "@superfluid-finance/ethereum-contracts/contracts/libs/SuperfluidPoolDeployerLibrary.sol": {
+//                SuperfluidPoolDeployerLibrary: addr("f30")
+//            },
+//        };
     }
 }
 
--- packages/hot-fuzz/truffle-config.js
+++ packages/hot-fuzz/truffle-config.js
@@ -94,6 +94,8 @@ const M = (module.exports = {
         },
     },
 
+    contracts_build_directory: "./build/truffle",
+
     // Truffle DB is currently disabled by default; to enable it, change enabled:
     // false to enabled: true. The default storage location can also be
     // overridden by specifying the adapter settings, as shown in the commented code below.

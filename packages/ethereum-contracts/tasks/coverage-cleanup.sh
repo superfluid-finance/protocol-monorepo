35db06c0dd261ea90bc6160daa9f1632565f7c2e
Miao, ZhiCheng <zhicheng.miao@gmail.com>
2023-08-11 15:50:02 +0300

    fixes

--- packages/ethereum-contracts/tasks/coverage-cleanup.sh
+++ packages/ethereum-contracts/tasks/coverage-cleanup.sh
@@ -6,16 +6,16 @@ cd "$(dirname "$0")"/..
 
 # extract coverage for NFT contracts from forge coverage
 lcov -e ../../lcov.info \
-     'contracts/*' \
+     "packages/ethereum-contracts/contracts/*" \
      -o lcov.info
 
 # remove mocks, base super app, test and deployer contracts (see .solcover.js)
 lcov -r lcov.info \
-     'contracts/mocks/*' \
-     'contracts/apps/*Base*' \
-     'contracts/utils/*Test*' \
-     'contracts/utils/*Deploy*' \
-     'contracts/apps/SuperfluidLoaderLibrary.sol' \
+     "packages/ethereum-contracts/contracts/mocks/*" \
+     "packages/ethereum-contracts/contracts/apps/*Base*" \
+     "packages/ethereum-contracts/contracts/utils/*Test*" \
+     "packages/ethereum-contracts/contracts/utils/*Deploy*" \
+     "packages/ethereum-contracts/contracts/apps/SuperfluidLoaderLibrary.sol" \
      -o lcov.info
 
 # merge hardhat and forge coverage files

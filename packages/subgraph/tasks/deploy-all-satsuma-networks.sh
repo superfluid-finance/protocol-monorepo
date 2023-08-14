21b824561c339cf79101f7179eb5aa7a8fe76947
mmd-afegbua <afegbuaismaila@gmail.com>
2023-08-14 10:05:12 +0100

    deploys network to satsuma

--- packages/subgraph/tasks/deploy-all-satsuma-networks.sh
+++ packages/subgraph/tasks/deploy-all-satsuma-networks.sh
@@ -4,7 +4,7 @@
 # $1 = the version label
 
 # TODO: use jq and get the networks from metadata.json networks file
-SATSUMA_NETWORKS=( "matic" "xdai" "eth-mainnet" "eth-sepolia" )
+SATSUMA_NETWORKS=( "matic" "xdai" "eth-mainnet" "eth-sepolia" "optimism-mainnet" "arbitrum-one")
 
 for i in "${SATSUMA_NETWORKS[@]}";do
     ./tasks/deploy-to-satsuma-network.sh "$1" "$i"

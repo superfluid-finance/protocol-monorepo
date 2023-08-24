f13d8d758de1d2bf0090e55703ef4796ab57f1e7
0xdavinchee <0xdavinchee@gmail.com>
2023-08-24 17:17:43 +0300

    Revert "node 20 as default"
    This reverts commit e6c8184bfc7ef069da5cf4fe4054ec8410ea1515.

--- flake.nix
+++ flake.nix
@@ -65,8 +65,8 @@
     node18DevInputs = nodeDevInputsWith pkgs.nodejs_18;
     node20DevInputs = nodeDevInputsWith pkgs.nodejs_20;
 
-    # minimum development shell
-    minimumDevInputs = commonDevInputs ++ ethDevInputs ++ node20DevInputs;
+    # minimem development shell
+    minimumDevInputs = commonDevInputs ++ ethDevInputs ++ node18DevInputs;
 
     # additional tooling for whitehat hackers
     whitehatInputs = with pkgs; [

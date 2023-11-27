b8927859ac2bb538746b7386700c9aa9429216f2
0xdavinchee <0xdavinchee@gmail.com>
2023-11-27 11:07:07 +0200

    fix broken unit test

--- packages/subgraph/src/utils.ts
+++ packages/subgraph/src/utils.ts
@@ -13,6 +13,7 @@ import {
     IndexSubscription,
     Token,
     TokenStatistic,
+    PoolMember,
 } from "../generated/schema";
 
 /**************************************************************************

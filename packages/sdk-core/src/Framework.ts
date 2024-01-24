267fb51bcc18d4dea40275286ddafba587c3c20b
0xdavinchee <0xdavinchee@gmail.com>
2024-01-24 15:06:41 +0200

    sdk-core framework fix + metadata update

--- packages/sdk-core/src/Framework.ts
+++ packages/sdk-core/src/Framework.ts
@@ -199,14 +199,9 @@ export default class Framework {
                         hostAddress: networkData.addresses.host,
                         cfaV1Address: networkData.addresses.cfaV1,
                         idaV1Address: networkData.addresses.idaV1,
-                        // @note TODO - remove the any once you add gdaV1 and gdaV1Forwarder to metadata
-                        // add idaV1Forwarder to metadata as well
-                        gdaV1Address:
-                            (networkData.addresses as any).gdaV1 ||
-                            networkData.addresses.idaV1,
+                        gdaV1Address: networkData.addresses.gdaV1,
                         gdaV1ForwarderAddress:
-                            (networkData.addresses as any).gdaV1Forwarder ||
-                            networkData.addresses.idaV1,
+                            networkData.addresses.gdaV1Forwarder,
                         governanceAddress,
                         cfaV1ForwarderAddress:
                             networkData.addresses.cfaV1Forwarder,

955589d1064955cb4e29fd1afcd8caa855e4af00
didi <git@d10r.net>
2024-01-30 18:16:23 +0100

    inlike function for getting gas settings in Framework.js to fix failing package building

--- packages/js-sdk/src/Framework.js
+++ packages/js-sdk/src/Framework.js
@@ -8,9 +8,7 @@ const ConstantFlowAgreementV1Helper = require("./ConstantFlowAgreementV1Helper")
 const InstantDistributionAgreementV1Helper = require("./InstantDistributionAgreementV1Helper");
 const fetch = require("node-fetch");
 // yes, this is dirty. truffle is deprecated, so this won't stick forever.
-const {
-    getGasConfig,
-} = require("../../ethereum-contracts/ops-scripts/libs/common");
+const truffleConfig = require("../../ethereum-contracts/truffle-config");
 
 const User = require("./User");
 
@@ -96,7 +94,36 @@ module.exports = class Framework {
             additionalContracts: this._options.additionalContracts,
             contractLoader: this._options.contractLoader,
             networkId: this.networkId,
-            gasConfig: getGasConfig(this.networkId),
+            // copy of ethereum-contracts/ops-scripts/libs/common.js:getGasConfig()
+            gasConfig: (networkId) => {
+                let gasConfig = {};
+
+                const networkConfig = Object.values(truffleConfig.networks)
+                    .filter((e) => e !== undefined)
+                    .find((e) => e.network_id === networkId);
+
+                if (networkConfig !== undefined) {
+                    // gas limit
+                    if (networkConfig.gas !== undefined) {
+                        gasConfig.gas = networkConfig.gas;
+                    }
+                    // legacy gas price
+                    if (networkConfig.gasPrice !== undefined) {
+                        gasConfig.gasPrice = networkConfig.gasPrice;
+                    }
+
+                    // EIP-1559 gas price
+                    if (networkConfig.maxPriorityFeePerGas !== undefined) {
+                        gasConfig.maxPriorityFeePerGas =
+                            networkConfig.maxPriorityFeePerGas;
+                    }
+                    if (networkConfig.maxFeePerGas !== undefined) {
+                        gasConfig.maxFeePerGas = networkConfig.maxFeePerGas;
+                    }
+                }
+
+                return gasConfig;
+            },
         });
 
         const resolverAddress =

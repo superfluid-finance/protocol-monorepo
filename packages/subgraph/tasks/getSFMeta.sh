dbeac8331b123da8cbfdb507e6edab8e9dd57675
0xdavinchee <0xdavinchee@gmail.com>
2024-02-02 13:17:22 +0200

    package version sfmeta

--- packages/subgraph/tasks/getSFMeta.sh
+++ packages/subgraph/tasks/getSFMeta.sh
@@ -1,3 +1,6 @@
+# This files is a bash script that is used to generate the src/meta.ignore.ts file.
+# This is used to fill out the SFMeta entity (see schema.graphql)
+# The different fields: `${SOME_FIELD}` must be set in the enivronment when running this script.
 #!/usr/bin/env bash
 BRANCH="$(git branch --show-current)"
 
@@ -5,4 +8,5 @@ cat > src/meta.ignore.ts << EOF
 export let commitHash = "${COMMIT_HASH}";
 export let configuration = "${CONFIGURATION}";
 export let branch = "${BRANCH}";
+export let packageVersion = "${PACKAGE_VERSION}";
 EOF

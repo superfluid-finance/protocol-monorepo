9bc32eca7b1b506c0a78f59dcfe8591e61123fa9
0xdavinchee <0xdavinchee@gmail.com>
2024-02-06 10:57:15 +0200

    Subgraph V1.7.0 Release (#1820)
    * package version sfmeta
    
    * [SUBGRAPH] getSFMeta.sh: added PACKAGE_VERSION support
    
    * bump version + create CHANGELOG.md
    
    * changelog 1.7.0
    
    ---------
    
    Co-authored-by: Miao, ZhiCheng <zhicheng.miao@gmail.com>

--- packages/subgraph/tasks/getSFMeta.sh
+++ packages/subgraph/tasks/getSFMeta.sh
@@ -1,8 +1,16 @@
+# This files is a bash script that is used to generate the src/meta.ignore.ts file.
+# This is used to fill out the SFMeta entity (see schema.graphql)
+# The different fields: `${SOME_FIELD}` must be set in the enivronment when running this script.
 #!/usr/bin/env bash
-BRANCH="$(git branch --show-current)"
 
-cat > src/meta.ignore.ts << EOF
+D="$(readlink -f "$(dirname "$0")")"
+
+BRANCH="$(git branch --show-current)"
+PACKAGE_VERSION="$(jq -r ".version" $D/../package.json)"
+
+cat > "$D"/../src/meta.ignore.ts << EOF
 export let commitHash = "${COMMIT_HASH}";
 export let configuration = "${CONFIGURATION}";
 export let branch = "${BRANCH}";
+export let packageVersion = "${PACKAGE_VERSION}";
 EOF

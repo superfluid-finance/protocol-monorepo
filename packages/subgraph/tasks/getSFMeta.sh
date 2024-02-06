85ae31f3b93df7f9119e5a10ab4cb6bec2abb369
Kaspar Kallas <kaspar@superfluid.finance>
2024-02-06 11:45:51 +0000

    use different version specifying for metadata

--- packages/subgraph/tasks/getSFMeta.sh
+++ packages/subgraph/tasks/getSFMeta.sh
@@ -1,12 +1,12 @@
+#!/usr/bin/env bash
 # This files is a bash script that is used to generate the src/meta.ignore.ts file.
 # This is used to fill out the SFMeta entity (see schema.graphql)
 # The different fields: `${SOME_FIELD}` must be set in the enivronment when running this script.
-#!/usr/bin/env bash
 
 D="$(readlink -f "$(dirname "$0")")"
 
 BRANCH="$(git branch --show-current)"
-PACKAGE_VERSION="$(jq -r ".version" $D/../package.json)"
+PACKAGE_VERSION="$(jq -r ".version" "$D"/../package.json)"
 
 cat > "$D"/../src/meta.ignore.ts << EOF
 export let commitHash = "${COMMIT_HASH}";

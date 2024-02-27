091bf3ee631271fd013c8ab85fd0f1b9597def2c
Kaspar Kallas <kaspar@superfluid.finance>
2024-02-27 11:39:12 +0200

    add some robustness to transaction serialization

--- packages/sdk-redux/src/reduxSlices/transactionTrackerSlice/trySerializeTransaction.ts
+++ packages/sdk-redux/src/reduxSlices/transactionTrackerSlice/trySerializeTransaction.ts
@@ -1,17 +1,34 @@
 import {SignatureLike} from '@ethersproject/bytes';
-import {ethers, UnsignedTransaction} from 'ethers';
+import {ethers, Transaction} from 'ethers';
 
 /**
  * The use-case arose from Gnosis Safe transaction serialization failing.
  */
 export const trySerializeTransaction = (
-    transaction: UnsignedTransaction,
+    transaction: Partial<Transaction>,
     signature?: SignatureLike
 ): string | undefined => {
     try {
         return ethers.utils.serializeTransaction(transaction, signature);
     } catch (error) {
         // This tends to happen with Gnosis Safe which changes the transaction response structure.
+
+        if (transaction.hash) {
+            // Check if the transaction hash contains a prefix (e.g. chainId) followed by colon
+            const parts = transaction.hash.split(':');
+            if (parts.length === 2) {
+                // Remove the prefix and set the plain transaction hash
+                transaction.hash = parts[1];
+                // Second attempt to serialize the transaction with the correct (?) hash
+                try {
+                    return ethers.utils.serializeTransaction(transaction, signature);
+                } catch {
+                    // Log the first error instead
+                    console.warn(error);
+                }
+            }
+        }
+
         console.warn(error);
     }
     return undefined;

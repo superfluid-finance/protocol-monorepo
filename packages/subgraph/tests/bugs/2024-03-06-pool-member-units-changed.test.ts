35c080bcd6ea84533994c749d7e674a37bc672f3
0xdavinchee <0xdavinchee@gmail.com>
2024-03-06 13:58:32 +0200

    fixes

--- packages/subgraph/tests/bugs/2024-03-06-pool-member-units-changed.test.ts
+++ packages/subgraph/tests/bugs/2024-03-06-pool-member-units-changed.test.ts
@@ -1,9 +1,10 @@
 import { handleMemberUnitsUpdated } from "../../src/mappings/superfluidPool";
 import { createMemberUnitsUpdatedEvent } from "../gdav1/gdav1.helper";
 import { Address, BigInt } from "@graphprotocol/graph-ts";
-import { alice, bob, charlie } from "../constants";
-import { getPoolMemberID } from "../../src/utils";
+import { FAKE_INITIAL_BALANCE, alice, bob, charlie } from "../constants";
+import { BIG_INT_ZERO, getPoolMemberID } from "../../src/utils";
 import { assert, describe, test } from "matchstick-as";
+import { mockedGetAppManifest, mockedRealtimeBalanceOf } from "../mockedFunctions";
 
 describe("PoolMember not updating when units changed", () => {
     test("emit MemberUnitsUpdated event", () => {
@@ -13,18 +14,35 @@ describe("PoolMember not updating when units changed", () => {
         const poolMemberAccountAddress = Address.fromString(charlie);
         const poolMemberId = getPoolMemberID(poolAddress, poolMemberAccountAddress);
 
+        const flowRate = BigInt.fromI32(69);
         const oldUnits = BigInt.fromI32(100);
         const newUnits = BigInt.fromI32(200);
 
         const memberUnitsUpdatedEvent = createMemberUnitsUpdatedEvent(
             superTokenAddress,
-            poolMemberId,
+            poolMemberAccountAddress.toHexString(),
             oldUnits,
             newUnits
         );
         memberUnitsUpdatedEvent.block.number = BigInt.fromI32(300);
+        memberUnitsUpdatedEvent.address = poolAddress;
 
-        // // Act
+        mockedGetAppManifest(poolMemberAccountAddress.toHexString(), false, false, BIG_INT_ZERO);
+
+        mockedRealtimeBalanceOf(
+            superTokenAddress,
+            poolMemberAccountAddress.toHexString(),
+            memberUnitsUpdatedEvent.block.timestamp,
+            FAKE_INITIAL_BALANCE.minus(flowRate),
+            flowRate,
+            BIG_INT_ZERO
+        );
+
+        // Act
+        handleMemberUnitsUpdated(memberUnitsUpdatedEvent);
+
+        memberUnitsUpdatedEvent.block.number = BigInt.fromI32(420);
+
         handleMemberUnitsUpdated(memberUnitsUpdatedEvent);
 
         // // Assert

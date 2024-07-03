75e27622819309dc0b1953ff1580dd4adedba032
Miao, ZhiCheng <zhicheng.miao@gmail.com>
2024-07-03 18:57:44 +0300

    fix build

--- packages/ethereum-contracts/test/contracts/agreements/AgreementHelper.ts
+++ packages/ethereum-contracts/test/contracts/agreements/AgreementHelper.ts
@@ -4,7 +4,7 @@ import {ethers} from "hardhat";
 
 import IConstantFlowAgreementV1Artifact from "../../../build/hardhat/contracts/interfaces/agreements/IConstantFlowAgreementV1.sol/IConstantFlowAgreementV1.json";
 import IInstantDistributionAgreementV1Artifact from "../../../build/hardhat/contracts/interfaces/agreements/IInstantDistributionAgreementV1.sol/IInstantDistributionAgreementV1.json";
-import SuperfluidMockArtifact from "../../../build/hardhat/contracts/mocks/SuperfluidMock.sol/SuperfluidMock.json";
+import SuperfluidMockArtifact from "../../../build/hardhat/contracts/mocks/SuperfluidMock.sol/SuperfluidMock.t.json";
 import TestEnvironment from "../../TestEnvironment";
 
 import {

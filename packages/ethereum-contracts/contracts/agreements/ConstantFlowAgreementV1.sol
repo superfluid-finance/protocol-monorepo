1349e67db5a3bf9330f84b65b6cf9fee138d0c78
didi <git@d10r.net>
2024-07-18 15:45:08 +0200

    ?

--- packages/ethereum-contracts/contracts/agreements/ConstantFlowAgreementV1.sol
+++ packages/ethereum-contracts/contracts/agreements/ConstantFlowAgreementV1.sol
@@ -6,6 +6,7 @@ import {
     ISuperfluidGovernance,
     ISuperApp,
     ISuperfluidToken,
+    IConstantFlowAgreementV1,
     FlowOperatorDefinitions,
     SuperAppDefinitions,
     ContextDefinitions,

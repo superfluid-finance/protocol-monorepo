4eedbac570ca5bd34a6516292351f809ae206f5f
Miao ZhiCheng <miao@superfluid.finance>
2024-08-26 18:53:21 +0300

    Workflow caching with nix & faster pre-commit hook (#1929)
    * workflow: caching using mk-cache-key.nix and github cache
    
    This allows testing same code only once.
    
    - composite action: .github/actions/setup-workflow-base/action.yml
    - caching enalbed workflows:
      - ethereum-contracts
      - sdk-core
      - solidity-semantic-money
      - spec-haskell
      - hot-fuzz
      - automations
      - subgraph
    
    * new pre-commit hook
    
    - remove pre-commit script from all workflows,
    - instead, putting workflow filtering in the pre-commit hook.

---
+++ default.nix
@@ -0,0 +1,12 @@
+{
+  halfBoardModule = {
+    includedFiles = [
+      # development tooling defined in nix
+      ./flake.nix
+      ./flake.lock
+      # managing the npm dependencies with yarna
+      ./package.json
+      ./yarn.lock
+    ];
+  };
+}

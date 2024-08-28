8141e12019fc3ba6e2e4f99cecf0ac39f3866da3
Miao, ZhiCheng <zhicheng.miao@gmail.com>
2024-08-28 19:46:00 +0300

    fix build

--- packages/subgraph/tasks/setup-graph-node.sh
+++ packages/subgraph/tasks/setup-graph-node.sh
@@ -17,26 +17,6 @@ if ! which jq >/dev/null 2>&1; then
     exit 1
 fi
 
-# Create the graph-node container
-docker compose up --no-start graph-node
-
-# Start graph-node so we can inspect it
-docker compose start graph-node
-
-# Identify the container ID
-CONTAINER_ID=$(docker container ls | grep graph-node | cut -d' ' -f1)
-
-# Inspect the container to identify the host IP address
-HOST_IP=$(docker inspect "$CONTAINER_ID" | jq -r .[0].NetworkSettings.Networks[].Gateway)
-
-echo "Host IP: $HOST_IP"
-
-# Inject the host IP into docker-compose.yml
-#sed -i -e "s/host.docker.internal/$HOST_IP/g" docker-compose.yml
-
-function stop_graph_node {
-    # Ensure graph-node is stopped
-    docker compose stop graph-node
-}
-
-trap stop_graph_node EXIT
+docker compose up --no-start
+DOCKER_HOST_IP=$(docker network inspect subgraph_default | jq -r '.[0].IPAM.Config[].Gateway')
+export DOCKER_HOST_IP
